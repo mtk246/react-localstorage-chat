e500b4a4ba5fe8b7be62d1359a8381af653b40f4
jbarretodev <jbarretodev6105@gmail.com>
2022-01-18 11:16:51 -0400

    fix

--- app/Http/Controllers/MetadataController.php
+++ app/Http/Controllers/MetadataController.php
@@ -8,8 +8,13 @@ use App\Models\Metadata;
 
 class MetadataController extends Controller
 {
+    /**
+     * @param array $data
+     * @param $user_id
+     * @param $emailUser
+     * @return void
+     */
     public static function saveLogAuditory(array $data,$user_id=null,$emailUser=null){
-        $user = null;
 
         if(!is_null($user_id)) $user = User::whereId($user_id)->first();
         else $user = User::whereEmail($emailUser)->first();
