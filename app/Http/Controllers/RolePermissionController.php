6e02266e7d6765f1f25cd10b8ceda371156174a3
jbarretodev <jbarretodev6105@gmail.com>
2022-01-26 13:56:28 -0400

    fix

--- app/Http/Controllers/RolePermissionController.php
+++ app/Http/Controllers/RolePermissionController.php
@@ -52,7 +52,7 @@ class RolePermissionController extends Controller
     {
         $rs = $this->RolePermissionRepository->getOnePermission($id);
 
-        return $rs ? response()->json($rs) : response()->json("permission not found",404);
+        return !is_null($rs) ? response()->json($rs) : response()->json("permission not found",404);
     }
 
     /**
