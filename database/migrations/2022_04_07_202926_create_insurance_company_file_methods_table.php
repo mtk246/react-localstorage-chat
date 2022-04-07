bd0ab7fa47d706f1efaba247804c477d88d93a3c
Henry Paredes <henryp2804@gmail.com>
2022-04-07 17:41:43 -0400

    added tables for preloaded data

---
+++ database/migrations/2022_04_07_202926_create_insurance_company_file_methods_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInsuranceCompanyFileMethodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('insurance_company_file_methods', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_method', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('insurance_company_file_methods');
+    }
+}
