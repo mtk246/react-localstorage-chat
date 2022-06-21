202327fee113f435f5e7d71ea16449ea7b70a0b3
Henry Paredes <henryp2804@gmail.com>
2022-06-21 07:51:20 -0400

    Database adjustments of the procedures module

---
+++ database/migrations/2022_06_20_065524_add_fields_to_company_procedure_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFieldsToCompanyProcedureTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('company_procedure', function (Blueprint $table) {
+            $table->float('price', 8, 2);
+            $table->float('price_percentage', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('company_procedure', function (Blueprint $table) {
+            $table->dropColumn('price');
+            $table->dropColumn('price_percentage');
+        });
+    }
+};
