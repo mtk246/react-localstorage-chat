60cc7ae21ab71a9a34451dee1201e44d8f9375ad
Henry Paredes <henryp2804@gmail.com>
2023-02-17 22:48:16 -0400

    MCC-919 #Update routes

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,7 @@ Route::prefix("v1")/*->middleware('audit')*/
 
     Route::prefix("user")->group(function(){
         Route::get("/get-all-server",[\App\Http\Controllers\UserController::class,'getServerAllUsers'])->middleware(['auth:api']);
+        Route::get("/get-list",[\App\Http\Controllers\UserController::class,'getList'])->middleware(['auth:api']);
         Route::get("/search/{date_of_birth?}/{first_name?}/{last_name?}/{ssn?}", [\App\Http\Controllers\UserController::class,'search']);
         Route::post("/",[\App\Http\Controllers\UserController::class,'createUser']);
         Route::get("/",[\App\Http\Controllers\UserController::class,'getAllUsers'])->middleware(['auth:api']);
