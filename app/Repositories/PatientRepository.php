16cf282b505beeedae7ff4998e7236b9f039815d
Henry Paredes <henryp2804@gmail.com>
2023-02-20 15:09:57 -0400

    MCC-943 #Data mapping for superuser in patient module

--- app/Repositories/PatientRepository.php
+++ app/Repositories/PatientRepository.php
@@ -414,9 +414,236 @@ class PatientRepository
             "privateNotes"
         ])->find($id);
 
-        if(is_null($patient)) return null;
+        $record = [
+            "id"                => $patient->id,
+            "code"              => $patient->code,
+            "profile"           => [
+                "ssn"            => $patient->user->profile->ssn ?? '',
+                "first_name"     => $patient->user->profile->first_name ?? '',
+                "middle_name"    => $patient->user->profile->middle_name ?? '',
+                "last_name"      => $patient->user->profile->last_name ?? '',
+                "date_of_birth"  => $patient->user->profile->date_of_birth ?? '',
+                "sex"            => $patient->user->profile->sex ?? '',
+            ],
+            "driver_license"    => $patient->driver_license ?? '',
+            "language"          => $patient->user->language ?? '',
 
-        return $patient;
+            "created_at"        => $patient->created_at,
+            "updated_at"        => $patient->updated_at,
+            "last_modified"     => $patient->last_modified,
+            "public_note"       => isset($patient->publicNote) ? $patient->publicNote->note : null,
+        ];
+
+        $record['billing_companies'] = [];
+        foreach ($patient->billingCompanies as $billingCompany) {
+            $addresses = Address::where([
+                "addressable_id"     => $patient->user->id,
+                "addressable_type"   => User::class,
+                "billing_company_id" => $billingCompany->id ?? $billingCompany
+            ])->get();
+            $contact = Contact::where([
+                "contactable_id"     => $patient->user->id,
+                "contactable_type"   => User::class,
+                "billing_company_id" => $billingCompany->id ?? $billingCompany
+            ])->first();
+            $private_note = PrivateNote::where([
+                "publishable_id"     => $patient->id,
+                "publishable_type"   => Patient::class,
+                "billing_company_id" => $billingCompany->id ?? $billingCompany
+            ])->first();
+            $companies = $patient->companies()->whereHas('billingCompanies', function ($query) use ($billingCompany) {
+                $query->where('billing_company_id', $billingCompany->id ?? $billingCompany);
+            })->get();
+
+            $insurance_policies = $patient->insurancePolicies()->whereHas('insurancePlan.billingCompanies', function ($query) use ($billingCompany) {
+                $query->where('billing_company_id', $billingCompany->id ?? $billingCompany);
+            })->get();
+
+            /** Change to private data */
+            $guarantor = $patient->guarantor;
+            $emergency_contacts = $patient->emergencyContacts;
+            $employments = $patient->employments;
+            $social_medias = $patient->user->profile->socialMedias;
+
+            if (isset($social_medias)) {
+                $patient_social_medias = [];
+                foreach ($social_medias as $social_media) {
+                    array_push($patient_social_medias, [
+                        "link" => $social_media->link,
+                        "social_network" => $social_media->socialNetwork->name ?? '',
+                        "social_network_id" => $social_media->social_network_id
+                    ]);
+                }
+            };
+
+            if (isset($guarantor)) {
+                $patient_guarantor = [
+                    "name" => $guarantor->name,
+                    "phone" => $guarantor->phone,
+                ];
+            };
+            if (isset($emergency_contacts)) {
+                $patient_emergency_contacts = [];
+                foreach ($emergency_contacts as $emergency_contact) {
+                    array_push($patient_emergency_contacts, [
+                        "name" => $emergency_contact->name,
+                        "cellphone" => $emergency_contact->cellphone,
+                        "relationship_id" => $emergency_contact->relationship_id,
+                        "relationship" => $emergency_contact->relationship->description,
+                    ]);
+                }
+            };
+
+            if (isset($employments)) {
+                $patient_employments = [];
+                foreach ($employments as $employment) {
+                    array_push($patient_employments, [
+                        "employer_name" => $employment->employer_name,
+                        "employer_address" => $employment->employer_address,
+                        "employer_phone" => $employment->employer_phone,
+                        "position" => $employment->position
+                    ]);
+                }
+            };
+
+            if (isset($addresses)) {
+                $patient_addresses = [];
+                foreach ($addresses as $address) {
+                    array_push($patient_addresses, [
+                        "zip"                      => $address->zip,
+                        "city"                     => $address->city,
+                        "state"                    => $address->state,
+                        "address"                  => $address->address,
+                        "country"                  => $address->country,
+                        "address_type_id"          => $address->address_type_id,
+                        "address_type"             => $address->addressType->name ?? '',
+                        "country_subdivision_code" => $address->country_subdivision_code
+                    ]);
+                }
+            };
+
+            if (isset($contact)) {
+                $patient_contact = [
+                    "fax"          => $contact->fax,
+                    "email"        => $contact->email,
+                    "phone"        => $contact->phone,
+                    "mobile"       => $contact->mobile,
+                    "contact_name" => $contact->contact_name,
+                ];
+            };
+
+            if (isset($companies)) {
+                $patient_companies = [];
+                foreach ($companies as $patient_company) {
+                    array_push($patient_companies, [
+                        "company_id" => $patient_company->id,
+                        "company"    => $patient_company->name,
+                        "med_num"    => $patient_company->pivot->med_num ?? '',
+                    ]);
+                }
+            }
+
+            if (isset($insurance_policies)) {
+                $patient_policies = [];
+                foreach ($insurance_policies as $patient_policy) {
+                    $patient_policy_subscriber = [];
+                    $subscriber = $patient_policy->subscribers->first();
+                    if (isset($subscriber)) {
+                        $address = Address::where([
+                            "addressable_id"     => $subscriber->id,
+                            "addressable_type"   => Subscriber::class,
+                            "billing_company_id" => $billingCompany->id ?? $billingCompany
+                        ])->first();
+                        $contact = Contact::where([
+                            "contactable_id"     => $subscriber->id,
+                            "contactable_type"   => Subscriber::class,
+                            "billing_company_id" => $billingCompany->id ?? $billingCompany
+                        ])->first();
+                        if (isset($address)) {
+                            $subscriber_address = [
+                                "zip"                      => $address->zip,
+                                "city"                     => $address->city,
+                                "state"                    => $address->state,
+                                "address"                  => $address->address,
+                                "country"                  => $address->country,
+                                "address_type_id"          => $address->address_type_id,
+                                "address_type"             => $address->addressType->name ?? '',
+                                "country_subdivision_code" => $address->country_subdivision_code
+                            ];
+                        };
+
+                        if (isset($contact)) {
+                            $subscriber_contact = [
+                                "fax"          => $contact->fax,
+                                "email"        => $contact->email,
+                                "phone"        => $contact->phone,
+                                "mobile"       => $contact->mobile,
+                                "contact_name" => $contact->contact_name,
+                            ];
+                        };
+                        array_push($patient_policy_subscriber, [
+                            "ssn" => $subscriber->ssn,
+                            "first_name" => $subscriber->first_name,
+                            "last_name"  => $subscriber->last_name,
+                            "date_of_birth" => $subscriber->date_of_birth,
+                            "relationship_id" => $subscriber->relationship_id,
+                            "relationship" => $subscriber->relationship->description ?? '',
+                            "address"         => isset($subscriber_address) ? $subscriber_address : null,
+                            "contact"           => isset($subscriber_contact) ? $subscriber_contact : null,
+                        ]);
+                    }
+
+                    array_push($patient_policies, [
+                        "policy_number" => $patient_policy->policy_number,
+                        "group_number"  => $patient_policy->group_number,
+                        "insurance_company_id" => $patient_policy->insurancePlan->insurance_company_id ?? '',
+                        "insurance_company" => ($patient_policy->insurancePlan->insuranceCompany->payer_id ?? '') . ' - ' . $patient_policy->insurancePlan->insuranceCompany->name ?? '',
+                        "insurance_plan_id"    => $patient_policy->insurance_plan_id ?? '',
+                        "insurance_plan"    => $patient_policy->insurancePlan->name ?? '',
+                        "type_responsibility_id"    => $patient_policy->type_responsibility_id ?? '',
+                        "type_responsibility"    => $patient_policy->typeResponsibility->code ?? '',
+                        "insurance_policy_type_id"  => $patient_policy->insurance_policy_type_id ?? '',
+                        "insurance_policy_type"  => $patient_policy->insurancePolicyType->description ?? '',
+                        "eff_date"  => $patient_policy->eff_date,
+                        "end_date"  => $patient_policy->end_date,
+                        "assign_benefits"   => $patient_policy->assign_benefits ?? false,
+                        "release_info"  => $patient_policy->release_info ?? false,
+                        "own_insurance" => $patient_policy->pivot->own_insurance ?? false,
+                        "subscriber"    => $patient_policy_subscriber ?? null,
+                    ]);
+                }
+            }
+
+            array_push($record['billing_companies'], [
+                "id"   => $billingCompany->id,
+                "name" => $billingCompany->name,
+                "code" => $billingCompany->code,
+                "abbreviation" => $billingCompany->abbreviation,
+                "private_patient" => [
+                    "marital_status_id" => $patient->marital_status_id,
+                    "marital_status"    => $patient->maritalStatus->name,
+                    "marital"           => ($patient->maritalStatus->name ?? '' == 'Married') ? [
+                        "spuse_name"       => $patient->marital->spuse_name ?? '',
+                        "spuse_work"       => $patient->marital->spuse_work ?? '',
+                        "spuse_work_phone" => $patient->marital->spuse_work_phone ?? '',
+                    ] : null,
+                    "companies"          => isset($patient_companies) ? $patient_companies : null,
+                    "insurance_policies" => isset($patient_policies) ? $patient_policies : null,
+                    "need_guardian"      => isset($patient_guarantor) ? true : false,
+                    "guarantor"          => isset($patient_guarantor) ? $patient_guarantor : null,
+                    "emergency_contacts" => isset($patient_emergency_contacts) ? $patient_emergency_contacts : [],
+                    "employments"        => isset($patient_employments) ? $patient_employments : [],
+                    "social_medias"      => isset($patient_social_medias) ? $patient_social_medias : [],
+                    "status"             => $billingCompany->pivot->status ?? false,
+                    "private_note"       => $private_note->note ?? '',
+                    "addresses"          => isset($patient_addresses) ? $patient_addresses : null,
+                    "contact"            => isset($patient_contact) ? $patient_contact : null,
+                ]
+            ]);
+        }
+
+        return !is_null($record) ? $record : null;
+        return !is_null($patient) ? $patient : null;
     }
 
     /**
