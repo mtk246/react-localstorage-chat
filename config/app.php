4d5fcb18600a89614f6804515844d84dbf5c0817
mbello <mb@ciph3r.co>
2023-02-22 21:58:47 -0400

    register new provider

--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Roles\RolesServiceProvider::class,
+        App\Providers\ActionsServiceProvider::class,
 
     ],
 
