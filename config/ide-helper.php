0a35e75bb4e25c62666f3d910acb7a135e1165ba
mbello <mb@ciph3r.co>
2023-02-23 19:36:57 -0400

    add helpers functions to helpers add factory to helpers add dockblock to helpers run model ide helper wen migrate database

--- config/ide-helper.php
+++ config/ide-helper.php
@@ -48,7 +48,7 @@ return [
     |
     */
 
-    'include_factory_builders' => false,
+    'include_factory_builders' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -108,7 +108,7 @@ return [
     |
     */
 
-    'include_helpers' => false,
+    'include_helpers' => true,
 
     'helper_files' => [
         base_path() . '/vendor/laravel/framework/src/Illuminate/Support/helpers.php',
@@ -267,7 +267,7 @@ return [
     | magic methods and properties.
     |
     */
-    'include_class_docblocks' => false,
+    'include_class_docblocks' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -302,7 +302,7 @@ return [
     |
     */
     'post_migrate' => [
-        // 'ide-helper:models --nowrite',
+        'ide-helper:models --nowrite',
     ],
 
 ];
