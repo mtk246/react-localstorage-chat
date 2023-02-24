0ca51c75cb93e8819ec7f66427b2e2da611ca4ea
Henry Paredes <henryp2804@gmail.com>
2023-02-24 07:44:09 -0400

    MCC-647 Added instructions for processing insurance plan searches

--- app/Models/InsurancePlan.php
+++ app/Models/InsurancePlan.php
@@ -309,4 +309,14 @@ class InsurancePlan extends Model implements Auditable
             ];
         }
     }
+
+    public function scopeSearch($query, $search)
+    {
+        if ($search != "") {
+            return $query->whereRaw('LOWER(name) LIKE (?)', [strtolower("%$search%")])
+                         ->orWhereRaw('LOWER(code) LIKE (?)', [strtolower("%$search%")]);
+        }
+
+        return $query;
+    }
 }
