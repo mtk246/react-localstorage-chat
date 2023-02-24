2eb709f09c6f6096f5b1c8de479627722ec1c914
mbello <mb@ciph3r.co>
2023-02-24 18:06:22 -0400

    add todo message

--- app/Models/Copay.php
+++ app/Models/Copay.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/** @todo compleate model structure */
 class Copay extends Model
 {
     use HasFactory;
