450b8afe9262090166b7e0518fe496c749e37059
mbello <mb@ciph3r.co>
2023-02-24 17:19:36 -0400

    #MCC-963 add medicament parameters

--- app/Http/Requests/Models/Company/Medicament.php
+++ app/Http/Requests/Models/Company/Medicament.php
@@ -10,4 +10,33 @@ final class Medicament
         private array $medicament,
     ){ }
 
+    public function getCode(): string
+    {
+        return '';
+    }
+
+    public function getDate(): string
+    {
+        return $this->medicament['date'];
+    }
+
+    public function getDrugCode(): string
+    {
+        return $this->medicament['drug_code'];
+    }
+
+    public function getBatch(): string
+    {
+        return $this->medicament['batch'];
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->medicament['quantity'];
+    }
+
+    public function getFrequency(): int
+    {
+        return $this->medicament['frequency'];
+    }
 }
