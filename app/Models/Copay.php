76d53cd49f80e243e6c5d0253b40a42a7347a809
mbello <mb@ciph3r.co>
2023-02-24 18:05:02 -0400

    add relation to models

--- app/Models/Copay.php
+++ app/Models/Copay.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Copay extends Model
 {
     use HasFactory;
+
+    public function procedures(): BelongsToMany
+    {
+        return $this->belongsToMany(Procedure::class);
+    }
 }
