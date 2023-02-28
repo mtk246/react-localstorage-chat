bfa540651e8a5c3310d7263e885fcbeb825967ae
mbello <mb@ciph3r.co>
2023-02-27 20:21:44 -0400

    #MCC-963 add fillables to model #MCC-963 add parent to model #MCC-963 update code style

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -1,11 +1,33 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Medication extends Model
+final class Medication extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'code',
+        'date',
+        'drug_code',
+        'batch',
+        'quantity',
+        'frequency',
+    ];
+
+    public function companyProcedure(): BelongsTo
+    {
+        return $this->belongsTo(CompanyProcedure::class);
+    }
 }
