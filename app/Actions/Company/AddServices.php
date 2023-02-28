1ba7a4ccb3d0a440bc1d624d74f1b9c83af2d7ae
mbello <mb@ciph3r.co>
2023-02-27 20:05:01 -0400

    #MCC-963 add save medication logic

--- app/Actions/Company/AddServices.php
+++ app/Actions/Company/AddServices.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Actions\Company;
 
 use App\Exceptions\User\NotHaveBillingCompany;
+use App\Http\Requests\Models\Company\Medication;
 use App\Http\Requests\Models\Company\Service;
 use App\Models\BillingCompany;
 use App\Models\Company;
+use App\Models\CompanyProcedure;
 use App\Models\User;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
@@ -23,16 +25,21 @@ final class AddServices
 
             $procedures = $company->procedures();
 
-            $services->each(fn (Service $service) => $procedures
-                ->attach($service->getProcedureId(), [
+            $services->each(fn (Service $service) => tap(
+                CompanyProcedure::create([
                     'billing_company_id' => $service->getBillingCompanyId() ?? $billingCompany->id,
                     'mac_locality_id' => $service->getMacLocality(),
-                    'price' => $service['price'] ?? null,
-                    'price_percentage' => $service['price_percentage'] ?? null,
-                    'modifier_id' => $service['modifier_id'] ?? null,
-                    'insurance_label_fee_id' => $service['insurance_label_fee_id'] ?? null,
-                    'clia' => $service['clia'] ?? null,
-                ]));
+                    'price' => $service->getPrice(),
+                    'price_percentage' => $service->getPricePercentage(),
+                    'modifier_id' => $service->getModifierId(),
+                    'insurance_label_fee_id' => $service->getInsuranceLabelFeeId(),
+                    'clia' => $service->getClia(),
+                ]),
+                fn (CompanyProcedure $cProcedure) => $this->setMedications(
+                    $cProcedure,
+                    $service->getMedications(),
+                )
+            ));
 
             if ($user->cannot('super')) {
                 $procedures = $procedures->wherePivot('billing_company_id', $billingCompany->id);
@@ -64,5 +71,21 @@ final class AddServices
         }
 
         $query->detach();
+    }
+
+    private function setMedications(CompanyProcedure $cProcedure, Collection $medications): void
+    {
+        $medications->each(
+            fn (Medication $medication) => $cProcedure->medications()->updateOrCreate(
+                [
+                    'date' => $medication->getDate(),
+                    'drug_code' => $medication->getDrugCode(),
+                    'batch' => $medication->getBatch(),
+                    'quantity' => $medication->getQuantity(),
+                    'frequency' => $medication->getFrequency(),
+                ],
+                ['code' => $medication->getCode()],
+            )
+        );
     }
 }
