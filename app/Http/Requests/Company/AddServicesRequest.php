3f3eddd6e8205f4dbd6ae72b7431a64792bf1a1d
mbello <mb@ciph3r.co>
2023-02-28 08:05:07 -0400

    #MCC-963 clean code #MCC-963 fix bad get services metod

--- app/Http/Requests/Company/AddServicesRequest.php
+++ app/Http/Requests/Company/AddServicesRequest.php
@@ -1,19 +1,16 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Requests\Company;
 
 use App\Http\Requests\Models\Company\Service;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Collection;
 
-class AddServicesRequest extends FormRequest
+final class AddServicesRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -23,33 +20,34 @@ class AddServicesRequest extends FormRequest
      *
      * @return array<string, mixed>
      */
-    public function rules()
+    public function rules(): array
     {
         return [
-            'services'                          => ['required', 'array'],
-            'services.*.billing_company_id'     => ['nullable', 'integer'],
-            'services.*.procedure_id'           => ['required', 'integer'],
-            'services.*.modifier_id'            => ['nullable', 'integer'],
-            'services.*.price'                  => ['required', 'numeric'],
-            'services.*.mac'                    => ['nullable', 'string'],
-            'services.*.locality_number'        => ['nullable', 'numeric'],
-            'services.*.state'                  => ['nullable', 'string'],
-            'services.*.fsa'                    => ['nullable', 'string'],
-            'services.*.counties'               => ['nullable', 'string'],
+            'services' => ['required', 'array'],
+            'services.*.billing_company_id' => ['nullable', 'integer'],
+            'services.*.procedure_id' => ['required', 'integer'],
+            'services.*.modifier_id' => ['nullable', 'integer'],
+            'services.*.price' => ['required', 'numeric'],
+            'services.*.mac' => ['nullable', 'string'],
+            'services.*.locality_number' => ['nullable', 'numeric'],
+            'services.*.state' => ['nullable', 'string'],
+            'services.*.fsa' => ['nullable', 'string'],
+            'services.*.counties' => ['nullable', 'string'],
             'services.*.insurance_label_fee_id' => ['nullable', 'integer'],
-            'services.*.price_percentage'       => ['nullable', 'numeric'],
-            'services.*.clia'                   => ['nullable', 'string'],
-            'services.*.medications'            => ['nullable', 'array'],
-            'services.*.medications.*.date'     => ['required_with:services.*.medications', 'date'],
-            'services.*.medications.*.drug_code'=> ['required_with:services.*.medications', 'string'],
-            'services.*.medications.*.batch'    => ['required_with:services.*.medications', 'string'],
+            'services.*.price_percentage' => ['nullable', 'numeric'],
+            'services.*.clia' => ['nullable', 'string'],
+            'services.*.medications' => ['nullable', 'array'],
+            'services.*.medications.*.date' => ['required_with:services.*.medications', 'date'],
+            'services.*.medications.*.drug_code' => ['required_with:services.*.medications', 'string'],
+            'services.*.medications.*.batch' => ['required_with:services.*.medications', 'string'],
             'services.*.medications.*.quantity' => ['required_with:services.*.medications', 'integer'],
-            'services.*.medications.*.frequency'=> ['required_with:services.*.medications', 'integer'],
+            'services.*.medications.*.frequency' => ['required_with:services.*.medications', 'integer'],
         ];
     }
 
-    public function getservices(): Collection {
-        return collect($this->list)
-            ->map(fn(array $item) => new Service($item));
+    public function getservices(): Collection
+    {
+        return collect($this->input('services'))
+            ->map(fn (array $item) => new Service($item));
     }
 }
