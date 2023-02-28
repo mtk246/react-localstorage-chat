e666385c1d2e27889d692e04651be5413b0e753f
mbello <mb@ciph3r.co>
2023-02-27 20:23:07 -0400

    #MCC-963 add parent to medications

---
+++ database/migrations/2023_02_27_141321_add_company_procedure_id_to_medications.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('medications', function (Blueprint $table) {
+            $table->foreignId('company_procedure_id')
+                ->nullable()
+                ->constrained('company_procedure')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('medications', function (Blueprint $table) {
+        });
+    }
+};
