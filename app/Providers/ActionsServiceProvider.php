da983a57716e1d9b2048584ffaa76fa841f04e12
mbello <mb@ciph3r.co>
2023-03-03 15:09:56 -0400

    #MCC-963 register new action

--- app/Providers/ActionsServiceProvider.php
+++ app/Providers/ActionsServiceProvider.php
@@ -7,6 +7,7 @@ namespace App\Providers;
 use App\Actions\Company\AddContractFees;
 use App\Actions\Company\AddCopays;
 use App\Actions\Company\AddServices;
+use App\Actions\Company\GetCompany;
 use App\Repositories\CompanyRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,5 +19,6 @@ final class ActionsServiceProvider extends ServiceProvider
         $this->app->singleton(CompanyRepository::class, fn () => new CompanyRepository());
         $this->app->singleton(AddCopays::class, fn () => new AddCopays());
         $this->app->singleton(AddContractFees::class, fn () => new AddContractFees());
+        $this->app->singleton(GetCompany::class, fn () => new GetCompany());
     }
 }
