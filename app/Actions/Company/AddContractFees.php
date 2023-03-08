01534f269d004f85467ce9b12d77ca3570167ac0
mbello <mb@ciph3r.co>
2023-03-08 06:43:42 -0400

    #MCC-999 update actions namespace

--- app/Actions/Company/AddContractFees.php
+++ app/Actions/Company/AddContractFees.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Actions\Company;
 
-use App\Http\Requests\Casts\ContractFeePatiensCast;
-use App\Http\Requests\Casts\ContractFeesRequestCast;
+use App\Http\Casts\Company\ContractFeePatiensCast;
+use App\Http\Casts\Company\ContractFeesRequestCast;
 use App\Models\Company;
 use App\Models\ContractFee;
 use Illuminate\Support\Collection;
