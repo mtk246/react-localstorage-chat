0eaff4dcf26fb3ab29906fb00ced9cb61d85f6f0
mbello <mb@ciph3r.co>
2023-03-13 22:54:45 -0400

    #MCC-1016 register user agent checker lib

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Detection\MobileDetect;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
 final class AuthServiceProvider extends ServiceProvider
@@ -17,6 +19,15 @@ final class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
+
+    public function register(): void
+    {
+        $request = app(Request::class);
+        $this->app->singleton(MobileDetect::class, fn () => new MobileDetect(
+            $request->header(),
+            $request->header('User-Agent'),
+        ));
+    }
 
     public function boot(): void
     {
