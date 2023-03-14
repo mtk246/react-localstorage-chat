ed6a32237fbf6a28817f785ec91ccf436a52d141
mbello <mb@ciph3r.co>
2023-03-13 22:55:48 -0400

    #MCC-1016 update code style #MCC-1016 add type collum

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -1,15 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use OwenIt\Auditing\Contracts\Auditable;
 use OwenIt\Auditing\Auditable as AuditableTrait;
+use OwenIt\Auditing\Contracts\Auditable;
 
 /**
- * App\Models\Device
+ * App\Models\Device.
  *
  * @property int $id
  * @property string $ip
@@ -19,7 +21,8 @@ use OwenIt\Auditing\Auditable as AuditableTrait;
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property bool $status
  * @property string|null $code_temp
- * @property-read \App\Models\User|null $user
+ * @property \App\Models\User|null $user
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|Device newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Device newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Device query()
@@ -31,34 +34,38 @@ use OwenIt\Auditing\Auditable as AuditableTrait;
  * @method static \Illuminate\Database\Eloquent\Builder|Device whereStatus($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Device whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Device whereUserId($value)
+ *
  * @property string|null $ip_address
  * @property string|null $user_agent
  * @property string|null $last_login
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Audit> $audits
- * @property-read int|null $audits_count
+ * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Audit> $audits
+ * @property int|null $audits_count
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|Device whereIpAddress($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Device whereLastLogin($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Device whereUserAgent($value)
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Audit> $audits
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Audit> $audits
+ *
+ * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Audit> $audits
+ * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Audit> $audits
+ *
  * @mixin \Eloquent
  */
-class Device extends Model implements Auditable
+final class Device extends Model implements Auditable
 {
-    use HasFactory, AuditableTrait;
+    use HasFactory;
+    use AuditableTrait;
 
+    /** @var string[] */
     protected $fillable = [
-        "user_agent",
-        "ip_address",
-        "user_id",
-        "status",
-        "code_temp",
-        "last_login",
+        'user_agent',
+        'ip_address',
+        'user_id',
+        'status',
+        'code_temp',
+        'last_login',
+        'type',
     ];
 
-    /**
-     * @return BelongsTo
-     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
