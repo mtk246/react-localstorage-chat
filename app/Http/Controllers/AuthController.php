04eb86f168f45cec5ee00382432e72d1a95b6004
mbello <mb@ciph3r.co>
2023-03-15 07:29:50 -0400

    #MCC-1015 add check for ip range

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\LoginRequest;
 use App\Mail\LogNewDevice;
 use App\Models\Device;
 use App\Models\FailedLoginAttempt;
+use App\Models\IpRestriction;
+use App\Models\IpRestrictionMult;
 use App\Models\User;
 use Carbon\Carbon;
 use Detection\MobileDetect;
@@ -106,6 +108,10 @@ class AuthController extends Controller
     {
         $dataValidated = $request->validated();
         $dataValidated = $request->safe()->only(['email', 'password']);
+
+        if ($this->checkIpIsRestricted($request->ip())) {
+            return response()->json(['error' => __('You are not allowed to access this application')], 401);
+        }
 
         $user = User::where('email', $dataValidated['email'])->first();
 
@@ -403,6 +409,33 @@ class AuthController extends Controller
     public function refresh(Request $request): JsonResponse
     {
         return response()->json(auth()->refresh());
+    }
+
+    protected function checkIpIsRestricted(string $ip, int $billingCompanyId): bool
+    {
+        /** @var IpRestriction|null */
+        $ipRestriction = IpRestriction::query()
+            ->where('billing_company_id', $billingCompanyId)
+            ->with('ipRestrictionMults')
+            ->first();
+
+        if (is_null($ipRestriction)) {
+            return false;
+        }
+
+        return !$ipRestriction
+            ->ipRestrictionMults
+            ->reduce(function (bool $carry, IpRestrictionMult $item) use ($ip) {
+                if ($carry) {
+                    return true;
+                }
+
+                $beginRange = ip2long($item->begin_range);
+                $endRange = ip2long($item->end_range);
+                $ip = ip2long($ip);
+
+                return $ip >= $beginRange && $ip <= $endRange;
+            }, false);
     }
 
     protected function respondWithToken(string $token, string $ip, string $os): JsonResponse
