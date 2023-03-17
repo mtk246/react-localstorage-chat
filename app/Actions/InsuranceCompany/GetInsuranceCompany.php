b2703d5507d9040115f353ce18651473ed68fe2a
mbello <mb@ciph3r.co>
2023-03-17 10:04:24 -0400

    MCC-963 fixt bad name space

--- app/Actions/InsuranceCompany/GetInsuranceCompany.php
+++ app/Actions/InsuranceCompany/GetInsuranceCompany.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Actions\BillingCompany;
+namespace App\Actions\InsuranceCompany;
 
 use App\Models\InsuranceCompany;
 use Illuminate\Database\Eloquent\Collection;
