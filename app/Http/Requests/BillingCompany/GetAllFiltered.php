b1cceac9ac451a04779b5d0e20e0f6ca7935c23d
mbello <mb@ciph3r.co>
2023-03-16 21:27:08 -0400

    MCC-963 create basic request for filter request

---
+++ app/Http/Requests/BillingCompany/GetAllFiltered.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\BillingCompany;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+final class GetAllFiltered extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /** @return array<string, mixed> */
+    public function rules()
+    {
+        return [
+            'ids' => 'nullable|array',
+            'ids.*' => 'required|numeric',
+        ];
+    }
+}
