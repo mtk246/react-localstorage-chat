d6608c323cc17d2a4e548de453bc6709ace00209
mbello <mb@ciph3r.co>
2023-03-22 23:15:31 -0400

    MCC-963 add  request class for store exection IC

---
+++ app/Http/Requests/Company/StoreExectionICRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Company;
+
+use App\Http\Casts\Company\StoreExectionICRequestCast;
+use App\Http\Requests\Traits\HasCastedClass;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
+
+final class StoreExectionICRequest extends FormRequest
+{
+    use HasCastedClass;
+
+    protected string $castedClass = StoreExectionICRequestCast::class;
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /** @return array<string, mixed> */
+    public function rules()
+    {
+        return [
+            'billing_company_id' => [
+                Rule::requiredIf(Gate::check('is-admin')),
+                'integer',
+                'nullable',
+            ],
+            'store' => 'nullable|array',
+            'store.*.id' => 'accepted|integer',
+            'store.*.insurance_company_id' => [
+                'required',
+                'integer',
+                'exists:\App\Models\InsuranceCompany,id',
+            ],
+            'delete' => 'nullable|array',
+            'delete.*' => 'accepted|integer',
+        ];
+    }
+}
