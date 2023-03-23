e8dc4e35097219112a5f04945f175d70482a8a7a
mbello <mb@ciph3r.co>
2023-03-23 06:01:15 -0400

    MCC-963 add resourcer for update response

---
+++ app/Http/Resources/Company/CompanyDataResource.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Company;
+
+use App\Http\Casts\Company\UpdateCompanyRequestCast;
+use App\Http\Resources\RequestWrapedResource;
+use App\Models\Company;
+
+/** @property Company $resource */
+final class CompanyDataResource extends RequestWrapedResource
+{
+    /**
+     * @param UpdateCompanyRequestCast $request
+     *
+     * @return array<key, string>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'npi' => $this->resource->npi,
+            'ein' => $this->resource->ein,
+            'upin' => $this->resource->upin,
+            'clia' => $this->resource->clia,
+            'name' => $this->resource->name,
+            'nickname' => $this->resource
+                ->nicknames()
+                ->where('billing_company_id', $request->getBillingCompanyId())
+                ->first()?->nickname,
+            'taxonomies' => TaxonomiesResource::collection(
+                $this->resource->taxonomies()->orderBy('id')->get()
+            ),
+        ];
+    }
+}
---
+++ app/Http/Resources/Company/StatementResource.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Company;
+
+use App\Http\Casts\Company\StoreStatementRequestCast;
+use App\Models\CompanyStatement;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property CompanyStatement $resource
+ * @property StoreStatementRequestCast $request
+ */
+final class StatementResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array<key, string>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->resource->id,
+            'rule_id' => $this->resource->rule_id,
+            'when_id' => $this->resource->when_id,
+            'start_date' => $this->resource->start_date,
+            'end_date' => $this->resource->end_date,
+            'apply_to_ids' => $this->resource->apply_to_ids,
+            'created_at' => $this->resource->created_at,
+            'updated_at' => $this->resource->updated_at,
+        ];
+    }
+}
