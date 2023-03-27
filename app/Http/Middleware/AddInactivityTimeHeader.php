5a29ed36180005611bd15b41352e1cdd4dff8b7b
Henry Paredes <henryp2804@gmail.com>
2023-03-27 16:24:40 -0400

    Add inactivity time in header responnse

---
+++ app/Http/Middleware/AddInactivityTimeHeader.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+final class AddInactivityTimeHeader
+{
+    protected $webDowntime = 3600000;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = auth()->user();
+        $response = $next($request);
+        if (isset($user)) {
+            $inactivity_time = $this->webDowntime - ((\strtotime(Carbon::now()->toString()) - \strtotime($user->last_activity)) * 1000);
+            $response->headers->set('inactivity_time', $inactivity_time);
+        }
+        return $response;
+    }
+}
