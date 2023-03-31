405da33486fd7317500f560701f6857d3a10c3cf
mbello <mb@ciph3r.co>
2023-03-31 00:31:56 -0400

    MCC-1015 obtimise code MCC-1015 sincronize with documentation

--- app/Actions/User/Recovery.php
+++ app/Actions/User/Recovery.php
@@ -12,22 +12,24 @@ use Illuminate\Support\Facades\DB;
 
 final class Recovery
 {
-    public function user(RecoveryUserRequestCast $request): ?string
+    /** @return array<key, string> */
+    public function user(RecoveryUserRequestCast $request): ?array
     {
-        return DB::transaction(function () use ($request) {
-            $query = Profile::query()
-                ->whereFirstName($request->getFirstName())
-                ->whereLastName($request->getLastName())
+        /* @return array<key, string> */
+        return DB::transaction(function () use ($request): array {
+            $profile = Profile::query()
                 ->whereDateOfBirth($request->getBirthDate())
+                ->where('first_name', 'ilike', "%{$request->getFirstName()}%")
+                ->where('last_name', 'ilike', "%{$request->getLastName()}%")
                 ->when($request->getSsn(), function (Builder $query) use ($request) {
                     $ssn = $request->getSsn();
                     $ssnFormated = substr($ssn, 0, 1).'-'.substr($ssn, 1, strlen($ssn));
 
                     return $query->where('ssn', 'ilike', "%{$ssn}")
                         ->orWhere('ssn', 'ilike', "%{$ssnFormated}");
-                });
-
-            $profile = $query->with('user')->first(['id']);
+                })
+                ->with('user')
+                ->first(['id']);
 
             if (is_null($profile?->user)) {
                 return null;
@@ -37,7 +39,7 @@ final class Recovery
 
             $emailFormated = explode('@', $profile->user->email);
 
-            return middleRedactor($emailFormated[0], '*').'@'.middleRedactor($emailFormated[1], '*');
+            return ['email' => middleRedactor($emailFormated[0], '*').'@'.middleRedactor($emailFormated[1], '*')];
         });
     }
 }
