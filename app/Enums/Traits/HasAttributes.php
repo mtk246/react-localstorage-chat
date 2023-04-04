d14dd56440543c3c3cc0300262c907d83edde9e6
mbello <mb@ciph3r.co>
2023-04-04 05:47:23 -0400

    MCC-1048 add trait for access Atributes class

---
+++ app/Enums/Traits/HasAttributes.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasAttributes
+{
+    public function getAttribute(string $Attribute): string
+    {
+        $classAttributes = (new \ReflectionClassConstant(self::class, $this->name))->getAttributes();
+
+        $atribute = array_filter($classAttributes, function ($classAttribute) use ($Attribute) {
+            return $classAttribute->getName() === $Attribute;
+        });
+
+        return 0 < count($atribute)
+            ? $atribute[array_key_first($atribute)]?->getArguments()[0]
+            : throw new \Exception($this->getErrorMessage($Attribute));
+    }
+
+    protected function getErrorMessage(string $Attribute): string
+    {
+        return Str::replaceArray('?', [
+            Str::afterLast($Attribute, '\\'),
+            Str::afterLast(self::class, '\\'),
+            $this->name,
+        ], $this->errorMessage());
+    }
+
+    protected function errorMessage(): string
+    {
+        return 'Attribute "?" not found on enum "?" on case "?"';
+    }
+}
