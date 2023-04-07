1b1eee0245bac374114dbb6c4268a18d1b7304d2
mbello <mb@ciph3r.co>
2023-04-07 09:26:16 -0400

    MCC-963 add sinc for copays values MCC-963 change for update or create

--- app/Actions/Company/AddCopays.php
+++ app/Actions/Company/AddCopays.php
@@ -7,18 +7,21 @@ namespace App\Actions\Company;
 use App\Http\Casts\Company\CopayRequestCast;
 use App\Models\Company;
 use App\Models\Copay;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
 
 final class AddCopays
 {
-    public function invoke(Collection $copays, Company $company): Collection
+    public function invoke(Collection $copays, Company $company, User $user): Collection
     {
-        return DB::transaction(function () use ($copays, $company): Collection {
-            $company->copays()->update(['company_id' => null]);
+        return DB::transaction(function () use ($copays, $company, $user): Collection {
+            $this->syncCopays($company, $copays, $user->billingCompanies->first()?->id);
 
             $copays->each(fn (CopayRequestCast $copayData) => tap(
-                Copay::create([
+                Copay::query()->updateOrCreate([
                     'billing_company_id' => $copayData->getBillingCompanyId(),
                     'insurance_plan_id' => $copayData->getInsurancePlanId(),
                     'insurance_company_id' => $copayData->getInsuranceCompanyId(),
@@ -38,5 +41,22 @@ final class AddCopays
         $proceduresIds->each(
             fn (int $procedureId) => $copay->procedures()->attach($procedureId)
         );
+    }
+
+    private function syncCopays(Company $company, collection $services, ?int $billingCompanyId): void
+    {
+        Copay::query()
+            ->where('company_id', $company->id)
+            ->when(Gate::denies('is-admin'), function (Builder $query) use ($billingCompanyId): void {
+                $query->where('billing_company_id', $billingCompanyId);
+            })
+            ->whereNotIn('id', $services->map(
+                fn (CopayRequestCast $services) => $services->getId()
+            )->toArray())
+            ->get()
+            ->each(function (Copay $copay) {
+                $copay->procedures()->detach();
+                $copay->delete();
+            });
     }
 }
