6ec5f58059b1113598ebf9bc4c6828b8ff2cfa20
mbello <mb@ciph3r.co>
2023-04-10 19:17:11 -0400

    MCC-963 add resource to manage facility response

---
+++ app/Http/Resources/Company/FacilityResource.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Company;
+
+use App\Models\Facility;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @property Facility $resource */
+final class FacilityResource extends JsonResource
+{
+    /**
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array<key, string>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'billing_company_id' => $this->resource->pivot->billing_company_id,
+            'facility_id' => $this->resource->id,
+            'facility_type_id' => $this->resource->facility_type_id,
+            'billing_company' => $this->resource->billingCompanies()->find(
+                $this->resource->pivot->billing_company_id,
+            )->name ?? '',
+            'facility' => $this->resource->name,
+            'facility_type' => $this->resource->facilityType->type,
+        ];
+    }
+}
