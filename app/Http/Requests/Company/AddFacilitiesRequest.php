1f251802bf4064db55bb54a1f15bcedd2c823908
mbello <mb@ciph3r.co>
2023-04-10 19:18:03 -0400

    MCC-963 add wrapper to request class

--- app/Http/Requests/Company/AddFacilitiesRequest.php
+++ app/Http/Requests/Company/AddFacilitiesRequest.php
@@ -1,11 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Requests\Company;
 
+use App\Http\Casts\Company\FacilityCast;
+use App\Http\Requests\Traits\HasCastedClass;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
 
 class AddFacilitiesRequest extends FormRequest
 {
+    use HasCastedClass;
+
+    protected string $castedClass = FacilityCast::class;
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -24,9 +34,12 @@ class AddFacilitiesRequest extends FormRequest
     public function rules()
     {
         return [
-            'facilities'                      => ['required', 'array'],
-            'facilities.*.billing_company_id' => ['nullable', 'integer'],
-            'facilities.*.facility_id'        => ['required', 'integer'],
+            'facilities' => ['required', 'array'],
+            'facilities.*.billing_company_id' => [
+                Rule::requiredIf(Gate::check('is-admin')),
+                'integer',
+            ],
+            'facilities.*.facility_id' => ['required', 'integer'],
         ];
     }
 }
