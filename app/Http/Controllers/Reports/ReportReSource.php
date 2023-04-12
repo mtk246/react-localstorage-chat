da974874e1665b1f63c6582768c8125950b66607
mbello <mb@ciph3r.co>
2023-04-12 07:46:41 -0400

    MCC-1047 add store method configuration

--- app/Http/Controllers/Reports/ReportReSource.php
+++ app/Http/Controllers/Reports/ReportReSource.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Reports;
 
 use App\Actions\Reports\GetReport;
+use App\Actions\Reports\StoreAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Reports\GetAllRequest;
+use App\Http\Requests\Reports\StoreRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -19,9 +21,11 @@ final class ReportReSource extends Controller
         );
     }
 
-    public function store(Request $request): JsonResponse
+    public function store(StoreRequest $request, StoreAction $store): JsonResponse
     {
-        return response()->json();
+        return response()->json(
+            $store->invoke($request->casted())
+        );
     }
 
     public function show(string $id, Request $request, GetReport $getReport): JsonResponse
