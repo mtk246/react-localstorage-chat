4af6c73ad4844e905891ab6fdd7262f87a849f51
mbello <mb@ciph3r.co>
2023-04-12 09:20:22 -0400

    MCC-1047 add update/destroy actions

--- app/Http/Controllers/Reports/ReportReSource.php
+++ app/Http/Controllers/Reports/ReportReSource.php
@@ -6,9 +6,11 @@ namespace App\Http\Controllers\Reports;
 
 use App\Actions\Reports\GetReportAction;
 use App\Actions\Reports\StoreReportAction;
+use App\Actions\Reports\UpdateReportAction;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Reports\GetAllRequest;
 use App\Http\Requests\Reports\StoreRequest;
+use App\Models\Reports\Report;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -35,13 +37,25 @@ final class ReportReSource extends Controller
         );
     }
 
-    public function update(Request $request, $id): JsonResponse
+    public function update(StoreRequest $request, UpdateReportAction $update, Report $report): JsonResponse
     {
-        return response()->json();
+        if (!$this->authorize('update', $report)) {
+            abort(403);
+        }
+
+        return response()->json(
+            $update->invoke($request->casted(), $report)
+        );
     }
 
-    public function destroy($id): JsonResponse
+    public function destroy(Report $report): JsonResponse
     {
-        return response()->json();
+        if (!$this->authorize('destroy', $report)) {
+            abort(403);
+        }
+
+        $report->delete();
+
+        return response(status: 200)->json(['message' => 'Report deleted successfully.']);
     }
 }
