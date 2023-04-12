a60ceb244ba307e86c9aa4309a40591574375d50
mbello <mb@ciph3r.co>
2023-04-12 18:57:25 -0400

    MCC-1047 add report policy

---
+++ app/Policies/Reports/ReportPolicy.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies\Reports;
+
+use App\Models\Reports\Report;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Facades\Gate;
+
+final class ReportPolicy
+{
+    use HandlesAuthorization;
+
+    public function update(User $user, Report $report): bool
+    {
+        return $report->billingCompany
+            && $report->billingCompany->id === $user->billing_company_id;
+    }
+
+    public function destroy(User $user, Report $report): bool
+    {
+        return Gate::allows('is-admin')
+            ? true
+            : $report->billingCompany->id === $user->billing_company_id;
+    }
+}
