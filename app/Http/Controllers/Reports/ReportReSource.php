e814cb19018401c8e9199f573555712f304e7fa3
mbello <mb@ciph3r.co>
2023-04-13 06:59:50 -0400

    MCC-1047 change request type to update MCC-1047 add controller for get types

--- app/Http/Controllers/Reports/ReportReSource.php
+++ app/Http/Controllers/Reports/ReportReSource.php
@@ -7,9 +7,15 @@ namespace App\Http\Controllers\Reports;
 use App\Actions\Reports\GetReportAction;
 use App\Actions\Reports\StoreReportAction;
 use App\Actions\Reports\UpdateReportAction;
+use App\Enums\Reports\ReportType;
+use App\Enums\Reports\TagType;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Reports\GetAllRequest;
 use App\Http\Requests\Reports\StoreRequest;
+use App\Http\Requests\Reports\UpdateRequest;
+use App\Http\Resources\Enums\EnumResource;
+use App\Http\Resources\Enums\TypeResource;
+use App\Http\Resources\Reports\ReportTypeResource;
 use App\Models\Reports\Report;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -37,7 +43,7 @@ final class ReportReSource extends Controller
         );
     }
 
-    public function update(StoreRequest $request, UpdateReportAction $update, Report $report): JsonResponse
+    public function update(UpdateRequest $request, UpdateReportAction $update, Report $report): JsonResponse
     {
         if (!$this->authorize('update', $report)) {
             abort(403);
@@ -57,5 +63,19 @@ final class ReportReSource extends Controller
         $report->delete();
 
         return response(status: 200)->json(['message' => 'Report deleted successfully.']);
+    }
+
+    public function getReportTypes(): JsonResponse
+    {
+        return response()->json(
+            new EnumResource(collect(ReportType::cases()), ReportTypeResource::class),
+        );
+    }
+
+    public function getReportTags(): JsonResponse
+    {
+        return response()->json(
+            new EnumResource(collect(TagType::cases()), TypeResource::class),
+        );
     }
 }
