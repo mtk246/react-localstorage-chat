6a9f6cd2f48a324580edbe4cdf868004b53e07dd
mbello <mb@ciph3r.co>
2023-04-13 06:55:44 -0400

    MCC-1047 delete uneded interface MCC-1047 fix url atribute not set correctly

--- app/Enums/Reports/ReportType.php
+++ app/Enums/Reports/ReportType.php
@@ -7,55 +7,53 @@ namespace App\Enums\Reports;
 use App\Enums\Attributes\DescriptionAttribute;
 use App\Enums\Attributes\PublicAttribute;
 use App\Enums\Attributes\UrlAttribute;
-use App\Enums\Interfaces\TypeInterface;
 use App\Enums\Traits\EnumToArray;
 use App\Enums\Traits\HasAttributes;
 
-enum ReportType: int implements TypeInterface
+enum ReportType: int
 {
     use EnumToArray;
     use HasAttributes;
 
-    public const TABLEAU_URL = 'https://prod-useast-a.online.tableau.com/t/begento/views/';
-
-    #[UrlAttribute('ClaimsReportsPDFGenerator/General2', self::TABLEAU_URL)]
+    #[UrlAttribute('ClaimsReportsPDFGenerator/General2')]
     #[DescriptionAttribute('General Report')]
     #[PublicAttribute(true)]
     case GENERAL_REPORT = 1;
 
-    #[UrlAttribute('ClaimsReportsPDFGenerator/Paymenttransactionaudit', self::TABLEAU_URL)]
+    #[UrlAttribute('ClaimsReportsPDFGenerator/Paymenttransactionaudit')]
     #[DescriptionAttribute('Posted Payment Transaction Audit Report')]
     #[PublicAttribute(true)]
     case POSTED_PAYMENT_TRANSACTION_AUDIT_REPORT = 2;
 
-    #[UrlAttribute('ClaimsReportsPDFGenerator/Payments2', self::TABLEAU_URL)]
+    #[UrlAttribute('ClaimsReportsPDFGenerator/Payments2')]
     #[DescriptionAttribute('Payments')]
     #[PublicAttribute(true)]
     case PAYMENTS = 3;
 
-    #[UrlAttribute('ClaimsReportsPDFGenerator/Productivity', self::TABLEAU_URL)]
+    #[UrlAttribute('ClaimsReportsPDFGenerator/Productivity')]
     #[DescriptionAttribute('Productivity')]
     #[PublicAttribute(true)]
     case PRODUCTIVITY = 4;
 
-    #[UrlAttribute('ClaimsReportsPDFGenerator/DailyInsuranceResponsibilityAging', self::TABLEAU_URL)]
+    #[UrlAttribute('ClaimsReportsPDFGenerator/DailyInsuranceResponsibilityAging')]
     #[DescriptionAttribute('Daily Insurance Responsibility Aging')]
     #[PublicAttribute(true)]
     case DAILY_INSURANCE_RESPONSIBILITY_AGING = 5;
 
-    #[UrlAttribute('ClaimsReportsPDFGenerator/BadDebt', self::TABLEAU_URL)]
+    #[UrlAttribute('ClaimsReportsPDFGenerator/BadDebt')]
     #[DescriptionAttribute('Bad Debt Amount General Report')]
     #[PublicAttribute(true)]
     case BAD_DEBT_AMOUNT_GENERAL_REPORT = 6;
 
-    #[UrlAttribute('ClaimsReportsPDFGenerator/Charges/7620d2a6-b5e6-4bf5-96ff-fe5da3e4fa76/e8ee638c-fb84-4628-8000-f1f803ce426b', self::TABLEAU_URL)]
+    #[UrlAttribute('ClaimsReportsPDFGenerator/Charges/7620d2a6-b5e6-4bf5-96ff-fe5da3e4fa76/e8ee638c-fb84-4628-8000-f1f803ce426b')]
     #[DescriptionAttribute('Charges Report')]
     #[PublicAttribute(true)]
     case CHARGES_REPORT = 7;
 
     public function getUrl(): string
     {
-        return $this->getAttribute(UrlAttribute::class);
+        return 'https://prod-useast-a.online.tableau.com/t/begento/views/'
+            .$this->getAttribute(UrlAttribute::class);
     }
 
     public function getDescription(): string
