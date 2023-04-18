6c64a6fa74191f229b1cc2825aaf0541752ae47f
mbello <mb@ciph3r.co>
2023-04-18 16:57:13 -0400

    MCC-1013 add missing rules to list

--- app/Http/Requests/UpdateFacilityRequest.php
+++ app/Http/Requests/UpdateFacilityRequest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Http\Requests;
 
 use App\Models\Facility;
+use App\Rules\CountInArray;
 use App\Rules\IUnique;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rule;
 
 class UpdateFacilityRequest extends FormRequest
@@ -22,14 +24,27 @@ class UpdateFacilityRequest extends FormRequest
             'name' => ['required', 'string', new IUnique(Facility::class, 'name', $this->id)],
             'npi' => ['required', 'string'],
             'facility_type_id' => ['required', 'integer'],
-            'companies' => ['required', 'array'],
+            'companies' => [
+                'required',
+                'array',
+                'exists:\App\Models\Company,id',
+            ],
             'nickname' => ['nullable', 'string'],
             'abbreviation' => ['required', 'string', 'max:20'],
             'place_of_services' => ['nullable', 'array'],
 
-            'billing_company_id' => [Rule::requiredIf(auth()->user()->hasRole('superuser')), 'integer', 'nullable'],
+            'billing_company_id' => [
+                Rule::excludeIf(Gate::denies('is-admin')),
+                'required',
+                'integer',
+                'exists:\App\Models\BillingCompany,id',
+            ],
 
-            'taxonomies' => ['required', 'array'],
+            'taxonomies' => [
+                'required',
+                'array',
+                new CountInArray('primary', true, 1),
+            ],
             'taxonomies.*.tax_id' => ['required', 'string'],
             'taxonomies.*.name' => ['required', 'string'],
             'taxonomies.*.primary' => ['required', 'boolean'],
