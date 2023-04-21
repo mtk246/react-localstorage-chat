650c31857d74112fc064384258ecc235b4cd199a
mbello <mb@ciph3r.co>
2023-04-21 10:43:19 -0400

    fixt fix long key name

--- database/migrations/2022_08_21_171924_create_plan_status_service_type_codes_table.php
+++ database/migrations/2022_08_21_171924_create_plan_status_service_type_codes_table.php
@@ -1,11 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      *
@@ -16,7 +17,12 @@ return new class extends Migration
         Schema::create('plan_status_service_type_codes', function (Blueprint $table) {
             $table->id();
             $table->string('service_type_code');
-            $table->foreignId('claim_eligibility_plan_status_id')->constrained()->onDelete('restrict')->onUpdate('cascade');
+            $table->unsignedBigInteger('claim_eligibility_plan_status_id');
+            $table->foreign('claim_eligibility_plan_status_id', 'fk_psstc_ceps_id')
+                ->references('id')
+                ->on('claim_eligibility_plan_statuses')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
             $table->timestamps();
         });
     }
