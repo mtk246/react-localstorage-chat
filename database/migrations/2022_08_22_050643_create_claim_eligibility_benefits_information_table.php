050bdf57237be18c08ea74e6de90e0df80b61d1b
mbello <mb@ciph3r.co>
2023-04-21 10:37:59 -0400

    fixt fix long key name

--- database/migrations/2022_08_22_050643_create_claim_eligibility_benefits_information_table.php
+++ database/migrations/2022_08_22_050643_create_claim_eligibility_benefits_information_table.php
@@ -1,11 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      *
@@ -26,7 +27,12 @@ return new class extends Migration
             $table->string('benefit_amount')->nullable();
             $table->string('benefits_date_information')->nullable();
             $table->string('additional_information')->nullable();
-            $table->foreignId('claim_eligibility_id')->constrained()->onDelete('restrict')->onUpdate('cascade');
+            $table->unsignedBigInteger('claim_eligibility_id');
+            $table->foreign('claim_eligibility_id', 'fk_cebio_claim_eligibility_id')
+                ->references('id')
+                ->on('claim_eligibilities')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
             $table->timestamps();
         });
     }
