b58da1e2f9cca92004ee3c5d8130a8f0aeffcaaa
Henry Paredes <henryp2804@gmail.com>
2023-04-24 18:01:16 -0400

    Fix in headers inactivity time

--- app/Http/Middleware/AddInactivityTimeHeader.php
+++ app/Http/Middleware/AddInactivityTimeHeader.php
@@ -24,9 +24,9 @@ final class AddInactivityTimeHeader
         if (isset($user)) {
             $inactivity_time = $this->webDowntime - ((\strtotime(Carbon::now()->toString()) - \strtotime($user->last_activity)) * 1000);
             if ($user->email == 'mr@ciph3r.co') {
-                $response->headers->set('inactivity_time', 120000);
+                $response->headers->set('inactivity-time', 120000);
             } else {
-                $response->headers->set('inactivity_time', $inactivity_time);
+                $response->headers->set('inactivity-time', $inactivity_time);
             }
         }
         return $response;
