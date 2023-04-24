73818127af50d01576509b94fe02a37f743288ab
mbello <mb@ciph3r.co>
2023-04-24 16:58:07 -0400

    add forge stagin update url

---
+++ app/Models/CustomShortcuts.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+final class CustomShortcuts extends Model
+{
+    /** @var string[] */
+    protected $fillable = [
+        'key',
+        'keyboard_shortcut_id',
+        'shortcutable_type',
+        'shortcutable_id',
+    ];
+
+    public function keyboardShortcut(): BelongsTo
+    {
+        return $this->belongsTo(KeyboardShortcut::class);
+    }
+
+    public function billingCompany(): MorphTo
+    {
+        return $this->morphTo(BillingCompany::class, 'shortcutable');
+    }
+
+    public function user(): MorphTo
+    {
+        return $this->morphTo(User::class, 'shortcutable');
+    }
+}
