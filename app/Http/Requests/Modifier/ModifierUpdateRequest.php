5d689b83cdc88f536bba6370736fcba53e4fb469
mbello <mb@ciph3r.co>
2023-05-05 06:46:39 -0400

    MCC-1061 delete useles method MCC-1061 update comment

--- app/Http/Requests/Modifier/ModifierUpdateRequest.php
+++ app/Http/Requests/Modifier/ModifierUpdateRequest.php
@@ -8,21 +8,7 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ModifierUpdateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
+    /** @return array<key, string|array<object|string>> */
     public function rules()
     {
         return [
