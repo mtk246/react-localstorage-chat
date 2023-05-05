6d529999d3c60b9cf29c196677f66c52156c15a9
mbello <mb@ciph3r.co>
2023-05-05 06:48:21 -0400

    MCC-1061 add validation rule for clasification

--- app/Http/Requests/Modifier/ModifierUpdateRequest.php
+++ app/Http/Requests/Modifier/ModifierUpdateRequest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Requests\Modifier;
 
+use App\Enums\Modifier\ClassificationType;
+use App\Enums\Modifier\ModifierType;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
 
 class ModifierUpdateRequest extends FormRequest
 {
@@ -17,6 +20,9 @@ class ModifierUpdateRequest extends FormRequest
             'special_coding_instructions' => ['required', 'string'],
             'modifier_invalid_combinations' => ['nullable', 'array'],
             'modifier_invalid_combinations.*' => ['nullable', 'string', 'max:2'],
+            'classification' => ['required', new Enum(ClassificationType::class)],
+            'type' => ['required', new Enum(ModifierType::class)],
+            'description' => ['required', 'string'],
             'note' => ['required', 'string'],
         ];
     }
