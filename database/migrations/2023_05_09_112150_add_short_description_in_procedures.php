47e264c5c967d67c9cd779a2cc21d048664cef7a
mbello <mb@ciph3r.co>
2023-05-11 17:54:58 -0400

    MCC-1059 fix unsuported feature for mysql

--- database/migrations/2023_05_09_112150_add_short_description_in_procedures.php
+++ database/migrations/2023_05_09_112150_add_short_description_in_procedures.php
@@ -10,7 +10,7 @@ return new class() extends Migration {
     public function up(): void
     {
         Schema::table('procedures', function (Blueprint $table) {
-            $table->tinyText('short_description')->default('');
+            $table->tinyText('short_description');
         });
     }
 
