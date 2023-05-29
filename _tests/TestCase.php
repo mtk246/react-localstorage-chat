973a071aa790c5d223069bb37f109acdb79567ca
Kevin Perez <kevinalbertoperez@gmail.com>
2023-05-29 13:58:12 -0400

    Kperez changes on test folder 3

---
+++ _tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use App\Models\User;
+use App\Roles\Models\Role;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+    // public function setUp(): void
+    // {
+    //     parent::setUp();
+
+    //     Artisan::call('migrate:refresh');
+    //     Artisan::call('db:seed --class=RoleSeeder');
+
+    //     $user = User::factory()->withProfile()->create();
+
+    //     $role = Role::where('slug', 'superuser')->first();
+    //     $user->attachRole($role);
+        
+    //     $this->actingAs($user);
+    // }
+}
