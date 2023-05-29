7883f2dea3401bfbfe95ba6c52d81c337e32bd1f
Kevin Perez <kevinalbertoperez@gmail.com>
2023-05-29 08:49:23 -0400

    Kperez check on test files 3

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,7 +15,7 @@ class ExampleTest extends TestCase
      */
     public function testExample()
     {
-        response = $this->get('/');
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
