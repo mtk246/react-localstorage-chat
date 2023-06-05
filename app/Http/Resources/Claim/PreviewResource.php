f3bbe3fa7b9210dec83733a9ad05f04923aac17c
Henry Paredes <henryp2804@gmail.com>
2023-06-05 12:28:04 -0400

    Fix size text in address in claim institutional

--- app/Http/Resources/Claim/PreviewResource.php
+++ app/Http/Resources/Claim/PreviewResource.php
@@ -587,15 +587,15 @@ final class PreviewResource extends JsonResource
 
         return [
             '1' => [
-                'name' => $company->name ?? '',
-                'address1' => $companyAddress->address ?? '',
+                'name' => substr($company->name ?? '', 0, 30),
+                'address1' => substr($companyAddress->address ?? '', 0, 30),
                 'address2' => substr($companyAddress->city ?? '', 0, 24),
                 'state' => substr($companyAddress->state ?? '', 0, 3),
                 'zip' => substr($companyAddress->zip ?? '', 0, 5),
             ],
             '2' => [
-                'name' => $company->name ?? '',
-                'address1' => $companyAddress->address ?? '',
+                'name' => substr($company->name ?? '', 0, 30),
+                'address1' => substr($companyAddress->address ?? '', 0, 30),
                 'address2' => substr($companyAddress->city ?? '', 0, 24),
                 'state' => substr($companyAddress->state ?? '', 0, 3),
                 'zip' => substr($companyAddress->zip ?? '', 0, 5),
