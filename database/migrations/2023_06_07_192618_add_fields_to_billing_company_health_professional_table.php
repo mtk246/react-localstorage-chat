21471aad8b1409c228553eae7c841534be736e5c
Ivan Castillo <ic@ciph3r.co>
2023-06-08 07:20:22 -0400

    MCC-1115 Added fields to pivot table between billing company and health professional

---
+++ database/migrations/2023_06_07_192618_add_fields_to_billing_company_health_professional_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('billing_company_health_professional', function (Blueprint $table) {
+            $table->string('npi_company')->nullable();
+            $table->boolean('is_provider')->default(false);
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('restrict')->onUpdate('cascade');
+            $table->foreignId('health_professional_type_id')->nullable()->constrained()->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('billing_company_health_professional', function (Blueprint $table) {
+            $table->dropColumn(['npi_company', 'is_provider']);
+
+            $table->dropForeign(['health_professional_type_id']);
+            $table->dropColumn('health_professional_type_id');
+
+            $table->dropForeign(['company_id']);
+            $table->dropColumn('company_id');
+        });
+    }
+};
