949707d1ba30fbcb3d062fdd4d420420d4a4a09a
maikel jose <mb@ciph3r.co>
2023-06-12 08:12:07 -0400

    MCC-1219 add ide helper coments MCC-1219 add missing relations

--- app/Models/Claims/Claim.php
+++ app/Models/Claims/Claim.php
@@ -6,10 +6,64 @@ namespace App\Models\Claims;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Laravel\Scout\Searchable;
 use OwenIt\Auditing\Auditable as AuditableTrait;
 use OwenIt\Auditing\Contracts\Auditable;
 
+/**
+ * App\Models\Claims\Claim.
+ *
+ * @property int $id
+ * @property string|null $qr_claim
+ * @property string|null $control_number
+ * @property string|null $submitter_name
+ * @property string|null $submitter_contact
+ * @property string|null $submitter_phone
+ * @property int|null $company_id
+ * @property int|null $facility_id
+ * @property int|null $patient_id
+ * @property string|null $claim_formattable_type
+ * @property int|null $claim_formattable_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property bool $validate
+ * @property bool $automatic_eligibility
+ * @property int|null $billing_provider_id
+ * @property int|null $service_provider_id
+ * @property int|null $referred_id
+ * @property int|null $referred_provider_role_id
+ * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Audit> $audits
+ * @property int|null $audits_count
+ * @property \App\Models\Claims\ClaimDemographicInformation|null $demographicInformation
+ * @property \App\Models\Claims\ClaimServices|null $services
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereAutomaticEligibility($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereBillingProviderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereClaimFormattableId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereClaimFormattableType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereCompanyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereControlNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereFacilityId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim wherePatientId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereQrClaim($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereReferredId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereReferredProviderRoleId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereServiceProviderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereSubmitterContact($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereSubmitterName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereSubmitterPhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Claim whereValidate($value)
+ *
+ * @mixin \Eloquent
+ */
 class Claim extends Model implements Auditable
 {
     use HasFactory;
@@ -26,11 +80,19 @@ class Claim extends Model implements Auditable
 
     /**
      * The insurance policies that belong to the Claim.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function insurancePolicies()
+    public function insurancePolicies(): BelongsToMany
     {
         return $this->belongsToMany(InsurancePolicy::class, 'claim_insurance_policy', 'claim_id', 'insurance_policy_id')->withPivot('order')->withTimestamps();
+    }
+
+    public function demographicInformation(): HasOne
+    {
+        return $this->hasOne(ClaimDemographicInformation::class);
+    }
+
+    public function services(): HasOne
+    {
+        return $this->hasOne(ClaimServices::class);
     }
 }
