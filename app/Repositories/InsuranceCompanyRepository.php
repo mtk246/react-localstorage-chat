7b0cab51c4601eeca4175ea31721823bc5313942
Henry Paredes <henryp2804@gmail.com>
2023-06-14 17:44:14 -0400

    MCC-1128 Fixed search by payer to record the insurance in multiple billing companies

--- app/Repositories/InsuranceCompanyRepository.php
+++ app/Repositories/InsuranceCompanyRepository.php
@@ -16,6 +16,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
 
 class InsuranceCompanyRepository
 {
@@ -42,7 +43,7 @@ class InsuranceCompanyRepository
                 ]);
             }
 
-            if (auth()->user()->hasRole('superuser')) {
+            if (Gate::check('is-admin')) {
                 $billingCompany = $data['billing_company_id'];
             } else {
                 $billingCompany = auth()->user()->billingCompanies->first();
@@ -415,7 +416,7 @@ class InsuranceCompanyRepository
             ->with('publicNote')
             ->first();
         if ($insurance) {
-            if (auth()->user()->hasRole('superuser')) {
+            if (Gate::check('is-admin')) {
                 $billingCompaniesException = $insurance->billingCompanies()
                     ->get()
                     ->pluck('id')
@@ -439,13 +440,13 @@ class InsuranceCompanyRepository
             }
         }
 
-        return !is_null($insurance) ? $insurance : null;
+        return !is_null($insurance) ? ['data' => $insurance, 'result' => true] : null;
     }
 
     public function getList(array $data)
     {
         try {
-            if (auth()->user()->hasRole('superuser')) {
+            if (Gate::check('is-admin')) {
                 $billingCompany = $data['billing_company_id'] ?? null;
             } else {
                 $billingCompany = auth()->user()->billingCompanies->first();
@@ -561,7 +562,7 @@ class InsuranceCompanyRepository
                 'file_method_id' => $data['insurance']['file_method_id'],
             ]);
 
-            if (auth()->user()->hasRole('superuser')) {
+            if (Gate::check('is-admin')) {
                 $billingCompany = $data['billing_company_id'];
             } else {
                 $billingCompany = auth()->user()->billingCompanies->first();
