965c4453bef899f3cd5802cff6f1e0335285eb01
maikel jose <mb@ciph3r.co>
2023-06-20 04:09:47 -0400

    MCC-1219 add array data

--- app/Http/Casts/Claims/DemographicInformationWrapper.php
+++ app/Http/Casts/Claims/DemographicInformationWrapper.php
@@ -5,11 +5,39 @@ declare(strict_types=1);
 namespace App\Http\Casts\Claims;
 
 use App\Http\Casts\CastsRequest;
+use Illuminate\Support\Collection;
 
 final class DemographicInformationWrapper extends CastsRequest
 {
     public function getData(): array
     {
-        return [];
+        return [
+            'type_of_medical_assistance' => $this->get('type_of_medical_assistance'),
+            'validate' => $this->get('validate'),
+            'automatic_eligibility' => $this->get('automatic_eligibility'),
+            'company_id' => $this->get('company_id'),
+            'facility_id' => $this->get('facility_id'),
+            'patient_id' => $this->get('patient_id'),
+            'prior_authorization_number' => $this->get('prior_authorization_number'),
+            'accept_assignment' => $this->get('accept_assignment'),
+            'patient_signature' => $this->get('patient_signature'),
+            'insured_signature' => $this->get('insured_signature'),
+            'outside_lab' => $this->get('outside_lab'),
+            'charges' => $this->get('charges'),
+            'employment_related_condition' => $this->get('employment_related_condition'),
+            'auto_accident_related_condition' => $this->get('auto_accident_related_condition'),
+            'auto_accident_place_state' => $this->get('auto_accident_place_state'),
+            'other_accident_related_condition' => $this->get('other_accident_related_condition'),
+        ];
+    }
+
+    public function getHealthProfessionals(): Collection
+    {
+        return $this->getCollect('health_professional_qualifier');
+    }
+
+    public function getDiagnoses(): Collection
+    {
+        return $this->getCollect('diagnoses');
     }
 }
