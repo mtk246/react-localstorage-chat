976ef9f4052eede8417a5c3719890477fc776ac0
maikel jose <mb@ciph3r.co>
2023-06-25 23:52:23 -0400

    MCC-1219 set format to class

--- app/Services/Claim/FileDictionary.php
+++ app/Services/Claim/FileDictionary.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Services\Claim;
 
+use App\Enums\Claim\FormatType;
+
 final class FileDictionary extends Dictionary
 {
+    protected string $format = FormatType::FILE;
 }
