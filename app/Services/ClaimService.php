5d95cba88d84095ea841c4b377879995563b648d
maikel jose <mb@ciph3r.co>
2023-06-25 23:52:53 -0400

    MCC-1219 add insurance company to creation script

--- app/Services/ClaimService.php
+++ app/Services/ClaimService.php
@@ -6,17 +6,18 @@ namespace App\Services;
 
 use App\Enums\Claim\FormatType;
 use App\Models\Claims\Claim;
+use App\Models\InsuranceCompany;
 use App\Services\Claim\DictionaryInterface;
 use App\Services\Claim\FileDictionary;
 use App\Services\Claim\X12Dictionary;
 
 final class ClaimService
 {
-    public function create(FormatType $formatType, Claim $claim): DictionaryInterface
+    public function create(FormatType $formatType, Claim $claim, InsuranceCompany $insuranceCompany): DictionaryInterface
     {
         return match ($formatType) {
-            FormatType::FILE => new FileDictionary($claim),
-            FormatType::X12 => new X12Dictionary($claim),
+            FormatType::FILE => new FileDictionary($claim, $insuranceCompany),
+            FormatType::X12 => new X12Dictionary($claim, $insuranceCompany),
             default => throw new \InvalidArgumentException('Invalid format type'),
         };
     }
