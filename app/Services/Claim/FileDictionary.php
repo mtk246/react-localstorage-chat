5fc25d1e324e20c700d2021e4c62fa09d74fcbf1
maikel jose <mb@ciph3r.co>
2023-06-26 07:04:16 -0400

    MCC-1219 add custom methods

--- app/Services/Claim/FileDictionary.php
+++ app/Services/Claim/FileDictionary.php
@@ -9,4 +9,20 @@ use App\Enums\Claim\FormatType;
 final class FileDictionary extends Dictionary
 {
     protected string $format = FormatType::FILE;
+
+    protected function getCompanyAttribute(string $key): string
+    {
+        return (string) $this->company->getAttribute($key);
+    }
+
+    protected function getPatientCompanyAttribute(string $key): string
+    {
+        return (string) $this->claim
+            ->demographicInformation
+            ->patient
+            ?->companies
+            ?->find($this->company->id ?? null)
+            ?->pivot
+            ?->med_num ?? '';
+    }
 }
