3aa386a94642000c3a92dc4b16cb95f2236aba75
maikel jose <mb@ciph3r.co>
2023-06-25 23:45:00 -0400

    MCC-1219 add missing dictionary methods

--- app/Services/Claim/Dictionary.php
+++ app/Services/Claim/Dictionary.php
@@ -6,45 +6,97 @@ namespace App\Services\Claim;
 
 use App\Enums\Claim\RuleType;
 use App\Models\Claims\Claim;
+use App\Models\InsuranceCompany;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 abstract class Dictionary implements DictionaryInterface
 {
     protected string $format;
 
-    public function __construct(protected readonly Claim $claim)
-    {
+    public function __construct(
+        protected readonly Claim $claim,
+        protected readonly InsuranceCompany $insuranceCompany,
+    ) {
     }
 
     public function translate(string $key): array|string
     {
         $config = config("claim.formats.{$this->claim->type->value}.{$this->format}.{$key}");
 
+        if (!$config) {
+            throw new \InvalidArgumentException('Invalid format key');
+        }
+
         return match ($config->type) {
-            RuleType::DATE->value => $this->getDateFormat(),
-            RuleType::BOOLEAN->value => $this->getBooleanFormat(),
-            RuleType::SINGLE->value => $this->getSingleFormat(),
-            RuleType::MULTIPLE->value => $this->getMultipleFormat(),
+            RuleType::DATE->value => $this->getDateFormat($config->value),
+            RuleType::BOOLEAN->value => $this->getBooleanFormat($config->value),
+            RuleType::SINGLE->value => $this->getSingleFormat($config->value),
+            RuleType::MULTIPLE->value => $this->getMultipleFormat($config->value),
             default => throw new \InvalidArgumentException('Invalid format type'),
         };
     }
 
-    protected function getMultipleFormat(): string|array
+    protected function getMultipleFormat(array $values): string
     {
-        return '';
+        $result = '';
+
+        foreach ($values as $value) {
+            $result .= (string) $this->getSingleFormat($value);
+        }
+
+        return $result;
     }
 
-    protected function getSingleFormat(): string|array
+    protected function getSingleFormat(string $value): string|Collection
     {
-        return '';
+        list($model, $key) = explode(':', $value);
+
+        if (!method_exists($this->claim, $model)) {
+            throw new \InvalidArgumentException('Invalid model getter');
+        }
+
+        $accesor = 'get'.Str::ucfirst($model).'Attribute';
+
+        return method_exists($this, $accesor)
+            ? $this->$accesor($key)
+            : $this->claim->$model->$key;
     }
 
-    protected function getDateFormat(): string
+    protected function getDateFormat(string $value): string
     {
-        return '';
+        list($model, $key, $format) = explode(':', $value);
+
+        if (!method_exists($this->claim, $model)) {
+            throw new \InvalidArgumentException('Invalid model getter');
+        }
+
+        $accesor = 'get'.Str::ucfirst($model).'Attribute';
+
+        return method_exists($this, $accesor)
+            ? $this->$accesor($key, $format)
+            : $this->claim->$model->$key
+                ->format($format);
     }
 
-    protected function getBooleanFormat(): string
+    protected function getBooleanFormat(string $value): string
     {
-        return '';
+        list($model, $key, $check) = explode(':', $value);
+
+        if (!method_exists($this->claim, $model)) {
+            throw new \InvalidArgumentException('Invalid model getter');
+        }
+
+        if ('null' == $check) {
+            $check = $this->claim->$model->$key;
+        }
+
+        $accesor = 'get'.Str::ucfirst($model).'Attribute';
+
+        if (method_exists($this, $accesor)) {
+            return $this->$accesor($key, $check);
+        }
+
+        return (bool) $check ? 'x' : '';
     }
 }
