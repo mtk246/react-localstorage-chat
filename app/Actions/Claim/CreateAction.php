9ecc9252c439b65cb1eae74be658cbf766a933a0
maikel jose <mb@ciph3r.co>
2023-06-30 07:42:09 -0400

    MCC-1219 fix wrong method at update claims

--- app/Actions/Claim/CreateAction.php
+++ app/Actions/Claim/CreateAction.php
@@ -4,26 +4,27 @@ declare(strict_types=1);
 
 namespace App\Actions\Claim;
 
+use App\Enums\Claim\ClaimType;
 use App\Http\Casts\Claims\StoreRequestWrapper;
 use App\Models\Claims\Claim;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\DB;
 
 final class CreateAction
 {
-    public function invoke(StoreRequestWrapper $claimData): Collection
+    public function invoke(StoreRequestWrapper $claimData): Claim
     {
-        return DB::transaction(tap(
+        return DB::transaction(fn () => tap(
             Claim::query()->create($claimData->getData()),
             function (Claim $claim) use ($claimData) {
                 $claim->setDemographicInformation($claimData->getDemographicInformation());
                 $claim->setServices($claimData->getClaimServices());
                 $claim->setInsurancePolicies($claimData->getPoliciesInsurances());
                 $claim->setStates($claimData->getStatus(), $claimData->getSubStatus());
-                $claim->query()->when($claim->type, function (Claim $query) use ($claimData) {
-                    $query->setAdditionalInformation($claimData->getAdditionalInformation());
-                });
+
+                if (ClaimType::INSTITUTIONAL->value == $claim->type) {
+                    $claim->setAdditionalInformation($claimData->getAdditionalInformation());
+                }
             }
-        )->load(['demographicInformation', 'services', 'policiesInsurances', 'aditionalInformation']));
+        )->load(['demographicInformation', 'services', 'insurancePolicies']));
     }
 }
