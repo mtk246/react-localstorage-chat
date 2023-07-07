3cb5b93e82b3990248b627748ee3c805e2807c5c
Henry Paredes <henryp2804@gmail.com>
2023-07-07 07:37:46 -0400

    MCC-1240 Fix in demographic information resource for institutional claim

--- database/migrations/2023_07_07_104848_update_fields_to_claim_demographic_table.php
+++ database/migrations/2023_07_07_104848_update_fields_to_claim_demographic_table.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     public function up(): void
     {
         Schema::table('claim_demographic', function (Blueprint $table) {
@@ -19,7 +18,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('claim_demographic', function (Blueprint $table) {
-            //
         });
     }
 };
