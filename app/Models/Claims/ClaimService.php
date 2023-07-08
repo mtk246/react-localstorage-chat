356857e137a74dce3ca7b741afdfc5010b8ee4a2
maikel jose <mb@ciph3r.co>
2023-07-08 00:58:51 -0400

    MCC-1219 update ide-helper

--- app/Models/Claims/ClaimService.php
+++ app/Models/Claims/ClaimService.php
@@ -12,13 +12,35 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
- * App\Models\Claims\ClaimServices.
+ * App\Models\Claims\ClaimService.
  *
  * @property \App\Models\Claims\Claim $claim
  *
- * @method static \Illuminate\Database\Eloquent\Builder|ClaimServices newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|ClaimServices newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|ClaimServices query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ClaimService newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ClaimService newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ClaimService query()
+ *
+ * @property int $id
+ * @property int $claim_id
+ * @property string|null $diagnosis_related_group_id
+ * @property string|null $non_covered_charges
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Database\Eloquent\Collection<int, Diagnosis> $diagnoses
+ * @property int|null $diagnoses_count
+ * @property TypeCatalog|null $diagnosisRelatedGroup
+ * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Claims\Services> $services
+ * @property int|null $services_count
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|ClaimService whereClaimId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ClaimService whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ClaimService whereDiagnosisRelatedGroupId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ClaimService whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ClaimService whereNonCoveredCharges($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ClaimService whereUpdatedAt($value)
+ *
+ * @property \Illuminate\Database\Eloquent\Collection<int, Diagnosis> $diagnoses
+ * @property \Illuminate\Database\Eloquent\Collection<int, \App\Models\Claims\Services> $services
  *
  * @mixin \Eloquent
  */
