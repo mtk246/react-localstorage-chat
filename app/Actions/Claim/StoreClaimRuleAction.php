08a58c36e7557437254f48da4926f6a15ec2be8a
maikel jose <mb@ciph3r.co>
2023-07-08 07:25:58 -0400

    MCC-1219 update wrapper name

--- app/Actions/Claim/StoreClaimRuleAction.php
+++ app/Actions/Claim/StoreClaimRuleAction.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Actions\Claim;
 
-use App\Http\Casts\Claim\RulesWrapper;
+use App\Http\Casts\Claim\StoreRulesWrapper;
 use App\Http\Resources\Claim\RuleResource;
 use App\Models\Claims\Rules;
 use Illuminate\Support\Facades\DB;
 
 final class StoreClaimRuleAction
 {
-    public function invoke(RulesWrapper $rulesWrapper): RuleResource
+    public function invoke(StoreRulesWrapper $rulesWrapper): RuleResource
     {
         return DB::transaction(function () use ($rulesWrapper) {
             return new RuleResource(Rules::query()->create($rulesWrapper->getRuleData()));
