7ea61988af738012735a9faf9e68668d6c1aa10c
Henry Paredes <henryp2804@gmail.com>
2023-07-08 14:41:54 -0400

    MCC-1240 Adjustments in claim resource for mapping additional fields

--- app/Http/Resources/Claim/ClaimBodyResource.php
+++ app/Http/Resources/Claim/ClaimBodyResource.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\Claim;
 
+use App\Models\Claims\ClaimCheckStatus;
+use App\Models\Claims\ClaimStatus;
+use App\Models\Claims\ClaimSubStatus;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Collection;
 
 final class ClaimBodyResource extends JsonResource
 {
@@ -34,28 +38,205 @@ final class ClaimBodyResource extends JsonResource
             'insurance_policies' => $this->resource->insurancePolicies->map(function ($model) {
                 return new InsurancePolicyResource($model);
             }),
-
-            /*
-             * @todo Pendiente por validar
-             *'last_modified' => $this->getLastModifiedAttribute(),
-             *'private_note' => $this->getPrivateNoteAttribute(),
-             *'status' => $this->getStatusAttribute(),
-             *'status_history' => $this->getStatusHistoryAttribute(),
-             *'notes_history' => $this->getNotesHistoryAttribute(),
-             *'billed_amount' => $this->getBilledAmountAttribute(),
-             *'amount_paid' => $this->getAmountPaidAttribute(),
-             *'past_due_date' => $this->getPastDueDateAttribute(),
-             *'date_of_service' => $this->getDateOfServiceAttribute(),
-             *'status_date' => $this->getStatusDateAttribute(),
-             *'insurance_company_id' => $this->getInsuranceCompanyIdAttribute(),
-             *'insurance_company' => $this->getInsuranceCompanyAttribute(),
-             *'insurance_plan' => $this->getInsurancePlanAttribute(),
-             *'user_created' => $this->getUserCreatedAttribute(),
-             *'type_responsibility' => $this->insurancePolicies()->first()?->typeResponsibility ?? null,
-             */
-
+            'last_modified' => $this->last_modified,
+            'private_note' => $this->private_note,
+            'status' => $this->getStatus(),
+            'status_history' => $this->getStatusHistory(),
+            'notes_history' => $this->getNotesHistory(),
+            'billed_amount' => $this->billed_amount,
+            'amount_paid' => $this->amount_paid ?? '0.00',
+            'past_due_date' => $this->past_due_date,
+            'date_of_service' => $this->getDateOfServiceAttribute(),
+            'status_date' => $this->getStatusDateAttribute(),
+            'insurance_company' => $this->getInsuranceCompany(),
+            'user_created' => $this->user_created,
             'created_at' => $this->resource->created_at,
             'updated_at' => $this->resource->updated_at,
+        ];
+    }
+
+    private function getStatus(): ClaimStatus
+    {
+        $data = $this->resource->status()
+            ->orderBy('claim_status_claim.id', 'desc')
+            ->first()
+            ?->setHidden(['pivot']);
+        $data['sub_status'] = $this->getSubstatus();
+
+        return $data;
+    }
+
+    private function getSubstatus(): ?ClaimSubStatus
+    {
+        return $this->resource->subStatus()
+            ->orderBy('claim_status_claim.id', 'desc')
+            ->first()
+            ?->setHidden(['pivot']);
+    }
+
+    private function getStatusHistory(): array
+    {
+        $records = [];
+        $recordSubstatus = [];
+        $history = $this->claimStatusClaims()
+                        ->orderBy('created_at', 'desc')
+                        ->orderBy('id', 'desc')->get() ?? [];
+        foreach ($history as $status) {
+            match ($status->claim_status_type) {
+                ClaimSubStatus::class => $this->setSubstatus($status, $records),
+                ClaimStatus::class => $this->setStatus($status, $records, $recordSubstatus),
+            };
+        }
+
+        return $records;
+    }
+
+    private function setSubStatus(Collection $status, &$recordSubstatus): void
+    {
+        $record = [];
+        $notes = [];
+        foreach ($status->privateNotes as $note) {
+            array_push(
+                $notes,
+                [
+                    'note' => $note->note,
+                    'created_at' => $note->created_at,
+                    'last_modified' => $note->last_modified,
+                ]
+            );
+        }
+        $record['notes_history'] = $notes;
+        $record['code'] = $status->claimStatus->code ?? '';
+        $record['name'] = $status->claimStatus->name ?? '';
+        $record['sub_status_date'] = $status->created_at;
+        $record['last_modified'] = $status->last_modified ?? '';
+        array_push($recordSubstatus, $record);
+    }
+
+    private function setStatus($status, &$records, &$recordSubstatus): void
+    {
+        $record = [];
+        $notes = [];
+        foreach ($status->privateNotes as $note) {
+            $check = ClaimCheckStatus::query()
+                ->where('private_note_id', $note->id)
+                ->first();
+            array_push(
+                $notes,
+                [
+                    'note' => $note->note,
+                    'created_at' => $note->created_at,
+                    'last_modified' => $note->last_modified,
+                    'check_status' => isset($check) ? [
+                        'response_details' => $check->response_details ?? '',
+                        'interface_type' => $check->interface_type ?? '',
+                        'interface' => $check->interface ?? '',
+                        'consultation_date' => $check->consultation_date ?? '',
+                        'resolution_time' => $check->resolution_time ?? '',
+                        'past_due_date' => $check->past_due_date ?? '',
+                    ] : null,
+                ]
+            );
+        }
+        $record['notes_history'] = $notes;
+        $record['status'] = $status->claimStatus->status ?? '';
+        $record['status_background_color'] = $status->claimStatus->background_color ?? '';
+        $record['status_font_color'] = $status->claimStatus->font_color ?? '';
+        $record['status_date'] = $status->created_at;
+        $record['sub_status_history'] = $recordSubstatus;
+        $record['last_modified'] = $status->last_modified ?? '';
+        array_push($records, $record);
+        $recordSubstatus = [];
+    }
+
+    private function getNotesHistory(): array
+    {
+        $records = [];
+        $recordSubstatus = [];
+        $history = $this->claimStatusClaims()
+                        ->orderBy('created_at', 'desc')
+                        ->orderBy('id', 'desc')->get() ?? [];
+        foreach ($history as $status) {
+            match ($status->claim_status_type) {
+                ClaimSubStatus::class => $this->setSubNote($status, $recordSubstatus),
+                ClaimStatus::class => $this->setNote($status, $records, $recordSubstatus),
+            };
+        }
+
+        return $records;
+    }
+
+    private function setSubNote($status, &$recordSubstatus): void
+    {
+        foreach ($status->privateNotes as $note) {
+            array_push(
+                $recordSubstatus,
+                [
+                    'status' => $status->claimStatus->name ?? '',
+                    'note' => $note->note,
+                    'created_at' => $note->created_at,
+                    'last_modified' => $note->last_modified,
+                ]
+            );
+        }
+    }
+
+    private function setNote($status, &$records, &$recordSubstatus): void
+    {
+        foreach ($recordSubstatus as $subNote) {
+            array_push(
+                $records,
+                [
+                    'note' => $subNote['note'],
+                    'created_at' => $subNote['created_at'],
+                    'last_modified' => $subNote['last_modified'],
+                    'check_status' => null,
+                    'status' => $subNote['status'],
+                    'status_background_color' => $status->claimStatus->background_color ?? '',
+                    'status_font_color' => $status->claimStatus->font_color ?? '',
+                ]
+            );
+        }
+        $recordSubstatus = [];
+        foreach ($status->privateNotes as $note) {
+            $check = ClaimCheckStatus::query()
+                ->where('private_note_id', $note->id)
+                ->first();
+            array_push(
+                $records,
+                [
+                    'note' => $note->note,
+                    'created_at' => $note->created_at,
+                    'last_modified' => $note->last_modified,
+                    'check_status' => isset($check) ? [
+                        'response_details' => $check->response_details ?? '',
+                        'interface_type' => $check->interface_type ?? '',
+                        'interface' => $check->interface ?? '',
+                        'consultation_date' => $check->consultation_date ?? '',
+                        'resolution_time' => $check->resolution_time ?? '',
+                        'past_due_date' => $check->past_due_date ?? '',
+                    ] : null,
+                    'status' => $status->claimStatus->status ?? '',
+                    'status_background_color' => $status->claimStatus->background_color ?? '',
+                    'status_font_color' => $status->claimStatus->font_color ?? '',
+                ]
+            );
+        }
+    }
+
+    private function getInsuranceCompany(): ?array
+    {
+        $policyPrimary = $this->resource
+            ->insurancePolicies()
+            ->wherePivot('order', 1)
+            ->first();
+
+        return [
+            'id' => $policyPrimary?->insurancePlan?->insuranceCompany?->id ?? '',
+            'name' => $policyPrimary?->insurancePlan?->insuranceCompany?->name ?? '',
+            'insurance_plan_id' => $policyPrimary?->insurancePlan?->id ?? '',
+            'insurance_plan' => $policyPrimary?->insurancePlan?->name ?? '',
+            'type_responsibility' => $policyPrimary?->typeResponsibility?->code ?? '',
         ];
     }
 }
