a58d376b03ab3a0516fcc9296d2a5b6c52bb73f8
maikel jose <mb@ciph3r.co>
2023-07-08 01:00:09 -0400

    MCC-1219 add insurance Company to rule

--- app/Models/Claim/Rules.php
+++ app/Models/Claim/Rules.php
@@ -9,7 +9,44 @@ use App\Models\BillingCompany;
 use App\Models\InsuranceCompany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * App\Models\Claim\Rules.
+ *
+ * @property int $id
+ * @property string $name
+ * @property RuleFormatType $format
+ * @property string $description
+ * @property int|null $billing_company_id
+ * @property array $rules
+ * @property array|null $parameters
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $insurance_company_id
+ * @property BillingCompany|null $billingCompany
+ * @property \Illuminate\Database\Eloquent\Collection<int, InsuranceCompany> $insuranceCompanies
+ * @property int|null $insurance_companies_count
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules whereBillingCompanyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules whereFormat($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules whereInsuranceCompanyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules whereParameters($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules whereRules($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rules whereUpdatedAt($value)
+ *
+ * @property \Illuminate\Database\Eloquent\Collection<int, InsuranceCompany> $insuranceCompanies
+ *
+ * @mixin \Eloquent
+ */
 final class Rules extends Model
 {
     use HasFactory;
@@ -21,6 +58,7 @@ final class Rules extends Model
         'format',
         'description',
         'billing_company_id',
+        'insurance_company_id',
         'rules',
         'parameters',
     ];
@@ -31,12 +69,12 @@ final class Rules extends Model
         'parameters' => 'array',
     ];
 
-    public function billingCompany()
+    public function billingCompany(): BelongsTo
     {
         return $this->belongsTo(BillingCompany::class);
     }
 
-    public function companies()
+    public function insuranceCompanies(): BelongsToMany
     {
         return $this->belongsToMany(InsuranceCompany::class, 'claim_rules');
     }
