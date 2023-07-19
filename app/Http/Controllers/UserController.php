f26cc8a219e6cb302e5a8d194359d324deaa7cd6
maikel jose <mb@ciph3r.co>
2023-07-19 12:01:40 -0400

    MCC-1161 fix worng error number

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -383,7 +383,7 @@ class UserController extends Controller
     {
         $rs = $this->userRepository->search($request);
 
-        return ($rs) ? response()->json($rs) : response()->json(__('Error, User already exists for this email'), 404);
+        return ($rs) ? response()->json($rs) : response()->json(__('Error, User already exists for this email'), 500);
     }
 
     public function updateLang(Request $request)
