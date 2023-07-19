ff24aaa97b1f2488d849faa74e381b10ead16229
maikel jose <mb@ciph3r.co>
2023-07-19 07:56:56 -0400

    MCC-1161 add comand to fix users in database

---
+++ app/Console/Commands/UserUpdater.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+final class UserUpdater extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:update-all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /** Execute the console command. */
+    public function handle(): int
+    {
+        $user = User::query()->with('billingCompanies')->where('billing_company_id', null)
+            ->whereHas('roles', function ($query) {
+                $query->where('name', 'Billing Manager');
+            })
+            ->get()
+            ->each(function ($user) {
+                $user->billing_company_id = $user->billingCompanies->first()->id;
+                $user->save();
+            });
+
+        dump($user->count());
+
+        return Command::SUCCESS;
+    }
+}
