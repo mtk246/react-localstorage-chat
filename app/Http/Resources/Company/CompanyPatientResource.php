1e3880528e4dadbe5f3e646290ffbcb4ae42e024
Henry Paredes <henryp2804@gmail.com>
2023-07-20 19:32:42 -0400

    MCC-1272 Updated mapping patient and billing company in company module

--- app/Http/Resources/Company/CompanyPatientResource.php
+++ app/Http/Resources/Company/CompanyPatientResource.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Resources\Company;
 
 use App\Http\Casts\Company\UpdatePatientRequest;
+use App\Models\BillingCompany;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /**
@@ -25,8 +26,23 @@ final class CompanyPatientResource extends JsonResource
         return [
             'id' => $this->resource->pivot->id,
             'patient_id' => $this->resource->pivot->patient_id,
+            'patient' => (isset($this->resource->user->profile))
+                ? [
+                    'id' => $this->resource->pivot->patient_id,
+                    'name' => $this->resource->code.' - '.
+                        $this->resource->user->profile->first_name.' '.
+                        substr($this->resource->user->profile->middle_name, 0, 1).' '.
+                        $this->resource->user->profile->last_name,
+                ]
+                : null,
             'med_num' => $this->resource->pivot->med_num ?? '',
             'billing_company_id' => $this->resource->pivot->billing_company_id,
+            'billing_company_id' => (isset($this->resource->pivot->billing_company_id))
+                ? [
+                    'id' => $this->resource->pivot->billing_company_id,
+                    'name' => BillingCompany::find($this->resource->pivot->billing_company_id)->value('name'),
+                ]
+                : null,
         ];
     }
 }
