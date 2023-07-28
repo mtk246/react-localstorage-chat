ee3ca1067d0105f8b07ea8d1f822a1e55bc815c1
Henry Paredes <henryp2804@gmail.com>
2023-07-28 18:00:17 -0400

    MCC-1272 Exception insurance action refactoring

--- app/Http/Resources/Company/ExceptionInsuranceResource.php
+++ app/Http/Resources/Company/ExceptionInsuranceResource.php
@@ -4,15 +4,14 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\Company;
 
-use App\Http\Casts\Company\StoreExceptionICRequestCast;
 use App\Http\Resources\RequestWrapedResource;
 use App\Models\ExceptionInsuranceCompany;
+use App\Models\InsurancePlan;
 
 /**
  * @property ExceptionInsuranceCompany $resource
- * @property StoreExceptionICRequestCast $request
  */
-final class ExceptionICResource extends RequestWrapedResource
+final class ExceptionInsuranceResource extends RequestWrapedResource
 {
     /**
      * @param \Illuminate\Http\Request $request
@@ -25,8 +24,11 @@ final class ExceptionICResource extends RequestWrapedResource
             'id' => $this->resource->id,
             'billing_company_id' => $this->resource->billing_company_id,
             'billing_company' => $this->resource->billingCompany->name ?? '',
-            'insurance_company_id' => $this->resource->insurance_company_id,
-            'insurance_company' => $this->resource->insuranceCompany?->name ?? '',
+            'insurance_plan_ids' => $this->resource->insurance_plan_ids ?? [],
+            'insurance_plans' => array_map(fn ($modelId) => [
+                'id' => $modelId,
+                'name' => InsurancePlan::find($modelId)?->name ?? '',
+            ], $this->resource->insurance_plan_ids ?? []),
             'created_at' => $this->resource->created_at,
             'updated_at' => $this->resource->updated_at,
         ];
