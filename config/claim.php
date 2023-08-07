dc7f071de8801320120e79af926e6e407adb23dc
Henry Paredes <henryp2804@gmail.com>
2023-08-07 09:33:20 -0400

    MCC-1237 MCC-1109 Settings in configuration file for json format

--- config/claim.php
+++ config/claim.php
@@ -9642,6 +9642,76 @@ return [
                     ],
                 ],
             ],
+            FormatType::JSON->value => [
+                'controlNumber' => [
+                    'type' => RuleType::SINGLE->value,
+                    'value' => 'claim:control_number',
+                ],
+                'tradingPartnerServiceId' => [
+                    'type' => RuleType::SINGLE->value,
+                    'value' => 'claim:tradingPartnerServiceId',
+                ],
+                'tradingPartnerName' => [
+                    'type' => RuleType::SINGLE->value,
+                    'value' => 'claim:tradingPartnerName',
+                ],
+                'usageIndicator' => [
+                    'type' => RuleType::SINGLE->value,
+                    'value' => 'claim:usageIndicator',
+                ],
+                'submitter' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:submitter',
+                ],
+                'receiver' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:receiver',
+                ],
+                'subscriber' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:subscriber',
+                ],
+                'dependent' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:dependent',
+                ],
+                'claimInformation' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:claimInformation',
+                ],
+                'payToAddress' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:payToAddress',
+                ],
+                'payToPlan' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:payToPlan',
+                ],
+                'payerAddress' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:payerAddress',
+                ],
+                'billing' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:billing',
+                ],
+                'referring' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:referring',
+                ],
+                'rendering' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:rendering',
+                ],
+                'ordering' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:ordering',
+                ],
+                'supervising' => [
+                    'type' => RuleType::SINGLE_ARRAY->value,
+                    'value' => 'claim:supervising',
+                ],
+            ],
         ],
     ],
 
