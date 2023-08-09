de2937f3458b5cd1cd2e0de7a32f3ce5e3b3e590
maikel jose <mb@ciph3r.co>
2023-08-09 17:40:55 -0400

    MCC-1521 fix bad enum range classifications

--- app/Enums/Diagnoses/ICD10/Specifics/CategoryIIIType.php
+++ app/Enums/Diagnoses/ICD10/Specifics/CategoryIIIType.php
@@ -19,37 +19,37 @@ enum CategoryIIIType: int implements ProcedureClassificationInterface
     use HasRangeAttribute;
 
     #[NameAttribute('Nutritional anemias')]
-    #[RangeAttribute('D50-D53')]
+    #[RangeAttribute('D50', 'D53')]
     #[PublicAttribute(true)]
     case NUTRITIONAL_ANEMIAS = 1;
 
     #[NameAttribute('Hemolytic anemias')]
-    #[RangeAttribute('D55-D59')]
+    #[RangeAttribute('D55', 'D59')]
     #[PublicAttribute(true)]
     case HEMOLYTIC_ANEMIAS = 2;
 
     #[NameAttribute('Aplastic and other anemias and other bone marrow failure syndromes')]
-    #[RangeAttribute('D60-D64')]
+    #[RangeAttribute('D60', 'D64')]
     #[PublicAttribute(true)]
     case APLASTIC_AND_OTHER_ANEMIAS = 3;
 
     #[NameAttribute('Coagulation defects, purpura and other hemorrhagic conditions')]
-    #[RangeAttribute('D65-D69')]
+    #[RangeAttribute('D65', 'D69')]
     #[PublicAttribute(true)]
     case COAGULATION_DEFECTS = 4;
 
-    #[NameAttribute('Other disorders of blood and blood-forming organs')]
-    #[RangeAttribute('D70-D77')]
+    #[NameAttribute('Other disorders of blood and blood', 'forming organs')]
+    #[RangeAttribute('D70', 'D77')]
     #[PublicAttribute(true)]
     case OTHER_BLOOD_DISORDERS = 5;
 
     #[NameAttribute('Intraoperative and postprocedural complications of the spleen')]
-    #[RangeAttribute('D78-D78')]
+    #[RangeAttribute('D78', 'D78')]
     #[PublicAttribute(true)]
     case SPLEEN_COMPLICATIONS = 6;
 
     #[NameAttribute('Certain disorders involving the immune mechanism')]
-    #[RangeAttribute('D80-D89')]
+    #[RangeAttribute('D80', 'D89')]
     #[PublicAttribute(true)]
     case IMMUNE_DISORDERS = 7;
 }
