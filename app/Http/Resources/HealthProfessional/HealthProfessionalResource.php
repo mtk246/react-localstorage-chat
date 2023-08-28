f94bce69e4fad353aaaa6d7871b2a2860eca6e8d
Ivan Castillo <ic@ciph3r.co>
2023-08-28 09:34:20 -0400

    MCC-1357 Applied laravel pint

--- app/Http/Resources/HealthProfessional/HealthProfessionalResource.php
+++ app/Http/Resources/HealthProfessional/HealthProfessionalResource.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\HealthProfessional;
 
-use Illuminate\Http\Resources\Json\JsonResource;
 use App\Models\Company;
+use Illuminate\Http\Resources\Json\JsonResource;
+
 final class HealthProfessionalResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
@@ -19,25 +21,25 @@ final class HealthProfessionalResource extends JsonResource
         return [
             'id' => $this->resource->id,
             'npi' => $this->resource->npi,
-            "created_at" => $this->resource->created_at,
-            "updated_at" => $this->resource->updated_at,
-            "code" => $this->resource->code,
-            "is_provider" => $this->resource->is_provider,
-            "npi_company" => $this->resource->npi_company,
-            "company_id" => $this->resource->company_id,
-            "nppes_verified_at" => $this->resource->nppes_verified_at,
-            "ein" => $this->resource->ein,
-            "upin" => $this->resource->upin,
-            "profile_id" => $this->resource->profile_id,
-            "user" => $this->resource->user,
-            "status" => $this->resource->status,
+            'created_at' => $this->resource->created_at,
+            'updated_at' => $this->resource->updated_at,
+            'code' => $this->resource->code,
+            'is_provider' => $this->resource->is_provider,
+            'npi_company' => $this->resource->npi_company,
+            'company_id' => $this->resource->company_id,
+            'nppes_verified_at' => $this->resource->nppes_verified_at,
+            'ein' => $this->resource->ein,
+            'upin' => $this->resource->upin,
+            'profile_id' => $this->resource->profile_id,
+            'user' => $this->resource->user,
+            'status' => $this->resource->status,
             'last_modified' => $this->resource->last_modified,
             'companies_providers' => $this->resource->companies_providers,
             'verified_on_nppes' => $this->resource->verified_on_nppes,
             'profile' => $this->resource->profile,
             'billing_companies' => $this->resource->billingCompanies,
             'taxonomies' => $this->resource->taxonomies,
-            'companies' => $this->getCompanies()
+            'companies' => $this->getCompanies(),
         ];
     }
 
@@ -72,8 +74,7 @@ final class HealthProfessionalResource extends JsonResource
 
     private function getCompanies()
     {
-
-        $companies = $this->resource->billingCompanies->map(function($bC){
+        $companies = $this->resource->billingCompanies->map(function ($bC) {
             return Company::find($bC->pivot->company_id);
         });
 
