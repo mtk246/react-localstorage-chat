4739bf8c81fff1cc0ba583205df391305cda559c
maikel jose <mb@ciph3r.co>
2023-09-04 20:56:02 -0400

    MCC-1209 update resource to new format

--- app/Http/Resources/Claim/RuleResource.php
+++ app/Http/Resources/Claim/RuleResource.php
@@ -4,24 +4,34 @@ declare(strict_types=1);
 
 namespace App\Http\Resources\Claim;
 
+use App\Enums\Claim\RuleFormatType;
 use App\Models\Claims\Rules;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /** @property Rules $resource */
 final class RuleResource extends JsonResource
 {
+    public $preserveKeys = true;
+
     /**
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
-            'insurance_company' => $this->resource->insuranceCompany(),
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+            'description' => $this->resource->description,
+            'insurance_plan' => $this->resource->insurancePlan,
             'format' => $this->resource->format,
-            'rules' => array_replace_recursive(
-                config("claim.formats.{$this->resource->format}"),
-                $this->resource->rules,
-            ),
+            'rules' => collect(config('claim.formats.'.(RuleFormatType::INSTITUTIONAL == $this->resource->format ? '2' : '1')))
+                ->map(fn ($format, $formatKey) => collect($format)
+                    ->mapWithKeys(function (array $rule, $key) use ($formatKey) {
+                        $rule['value'] = $this->resource->rules[$formatKey][$key] ?? $rule['value'];
+
+                        return [$key => $rule];
+                    })
+                ),
             'parameters' => $this->resource->parameters,
         ];
     }
