127674a96744e27d9bbcf51c2609c55db8b1ffeb
maikel jose <mb@ciph3r.co>
2023-09-12 08:21:45 -0400

    MCC-1598 add permission role resource

---
+++ app/Http/Controllers/Permissions/MembershipResource.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Permissions;
+
+use App\Actions\Permissions\StoreMembershipAction;
+use App\Actions\Permissions\UpdateMembershipAction;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Permissions\StoreMembershipRequest;
+use App\Http\Requests\Permissions\UpdateMembershipRequest;
+use App\Models\BillingCompany\MembershipRole;
+use Illuminate\Http\JsonResponse;
+
+final class MembershipResource extends Controller
+{
+    public function index(): JsonResponse
+    {
+        return response()->json();
+    }
+
+    public function store(StoreMembershipRequest $request, StoreMembershipAction $store): JsonResponse
+    {
+        return response()->json($store->invoke($request->casted()));
+    }
+
+    public function show(MembershipRole $role): JsonResponse
+    {
+        return response()->json($role);
+    }
+
+    public function update(UpdateMembershipRequest $request, MembershipRole $role, UpdateMembershipAction $upate): JsonResponse
+    {
+        return response()->json($upate->invoke($request->casted(), $role));
+    }
+
+    public function destroy(MembershipRole $role): JsonResponse
+    {
+        return response()->json($role->delete());
+    }
+}
