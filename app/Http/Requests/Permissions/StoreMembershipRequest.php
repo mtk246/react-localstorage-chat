089f94dcfef29fb59174f9139115df7c97259cdd
maikel jose <mb@ciph3r.co>
2023-09-12 08:17:23 -0400

    MCC-1598 add request for permission roles

---
+++ app/Http/Requests/Permissions/StoreMembershipRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Permissions;
+
+use App\Http\Casts\Permissions\StoreMembershipWrapper;
+use App\Http\Requests\Traits\HasCastedClass;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
+
+final class StoreMembershipRequest extends FormRequest
+{
+    use HasCastedClass;
+
+    protected string $castedClass = StoreMembershipWrapper::class;
+
+    /** @return array<string, mixed> */
+    public function rules()
+    {
+        return [
+            'billing_company_id' => [
+                Rule::excludeIf(Gate::denies('is-admin')),
+                'required',
+                'integer',
+                'exists:\App\Models\BillingCompany,id',
+            ],
+            'name' => ['required', 'string'],
+            'note' => ['nullable', 'string'],
+            'permissions' => ['required', 'array'],
+        ];
+    }
+}
