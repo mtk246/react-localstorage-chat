94df89f1305bf4be5c033d4a66385d6e370139de
maikel jose <mb@ciph3r.co>
2023-09-19 05:10:42 -0400

    MCC-1598 add paginnation to action

--- app/Actions/Permissions/GetRoleAction.php
+++ app/Actions/Permissions/GetRoleAction.php
@@ -4,20 +4,23 @@ declare(strict_types=1);
 
 namespace App\Actions\Permissions;
 
+use App\Facades\Pagination;
 use App\Models\BillingCompany\MembershipRole;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Gate;
 
 final class GetRoleAction
 {
-    public function getAll(): Collection
+    public function getAll(): LengthAwarePaginator
     {
         return MembershipRole::query()
             ->when(
                 Gate::denies('is-admin'),
                 fn (Builder $query) => $query->where('billing_company_id', \Auth::user()->billing_company_id),
             )
-            ->get();
+            ->with('billingCompany')
+            ->orderBy(Pagination::sortBy(), Pagination::sortDesc())
+            ->paginate(Pagination::itemsPerPage());
     }
 }
