f7e9a7a62faf6591d47f6ac1f376550f8b41c331
maikel jose <mb@ciph3r.co>
2023-09-19 05:16:13 -0400

    MCC-1598 update class names

--- app/Http/Controllers/Permissions/RoleResource.php
+++ app/Http/Controllers/Permissions/RoleResource.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Permissions;
 
 use App\Actions\Permissions\GetRoleAction;
-use App\Actions\Permissions\StoreMembershipAction;
-use App\Actions\Permissions\UpdateMembershipAction;
+use App\Actions\Permissions\StoreRoleAction;
+use App\Actions\Permissions\UpdateRoleAction;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Permissions\StoreMembershipRequest;
-use App\Http\Requests\Permissions\UpdateMembershipRequest;
+use App\Http\Requests\Permissions\StoreRoleRequest;
+use App\Http\Requests\Permissions\UpdateRoleRequest;
 use App\Models\BillingCompany\MembershipRole;
 use Illuminate\Http\JsonResponse;
 
@@ -20,7 +20,7 @@ final class RoleResource extends Controller
         return response()->json($get->getAll());
     }
 
-    public function store(StoreMembershipRequest $request, StoreMembershipAction $store): JsonResponse
+    public function store(StoreRoleRequest $request, StoreRoleAction $store): JsonResponse
     {
         return response()->json($store->invoke($request->casted()));
     }
@@ -30,7 +30,7 @@ final class RoleResource extends Controller
         return response()->json($role);
     }
 
-    public function update(UpdateMembershipRequest $request, MembershipRole $role, UpdateMembershipAction $upate): JsonResponse
+    public function update(UpdateRoleRequest $request, MembershipRole $role, UpdateRoleAction $upate): JsonResponse
     {
         return response()->json($upate->invoke($request->casted(), $role));
     }
