478fb7ede72b63f3b19693057b8bdfc7274af565
Henry Paredes <henryp2804@gmail.com>
2023-09-26 16:14:22 -0400

    MCC-1590 Fix url status claim

--- app/Actions/Claim/GetCheckStatusAction.php
+++ app/Actions/Claim/GetCheckStatusAction.php
@@ -5,22 +5,28 @@ declare(strict_types=1);
 namespace App\Actions\Claim;
 
 use App\Models\Claims\Claim;
+use App\Models\Company;
 use App\Models\Patient;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 
 final class GetCheckStatusAction
 {
-    public function invoke(string $token, Claim $claim): Claim
+    public function single(string $token, Claim $claim): Claim
     {
         return DB::transaction(function () use ($token, $claim) {
-            $claim = Claim::with(['patient', 'company', 'claimFormattable', 'claimFormattable.claimFormServices.typeOfService'])->find($id);
-            $patient = Patient::with([
-                'insurancePolicies' => function ($query) {
-                    $query->with('typeResponsibility');
-                },
-                'profile',
-            ])->find($claim->patient_id);
+            $patient = Patient::query()
+                ->with(
+                    [
+                        'insurancePolicies' => function ($query) {
+                            $query->with('typeResponsibility');
+                        },
+                        'profile',
+                    ]
+                )
+                ->find($claim->demographicInformation->patient_id);
+
+            $company = Company::query()->find($claim->demographicInformation->company_id);
 
             $insurancePolicy = $claim->insurancePolicies->first();
             $encounter = [];
@@ -33,32 +39,32 @@ final class GetCheckStatusAction
             }
             $encounter['serviceTypeCodes'] = $serviceCodes;
 
-            $dataReal = [
+            $body = [
                 'controlNumber' => $claim->control_number ?? '',
                 'tradingPartnerServiceId' => $insurancePolicy->insurancePlan->insuranceCompany->payer_id ?? null,
                 'tradingPartnerName' => $insurancePolicy->insurancePlan->insuranceCompany->name ?? null,
                 'provider' => [
-                    'organizationName' => $claim->company->name ?? null,
-                    'npi' => $claim->company->npi ?? null,
-                    'serviceProviderNumber' => $claim->company->sevices_number ?? null,
-                    'providerCode' => 'AD', // $claim->company->code ?? null,
-                    'referenceIdentification' => $claim->company->reference_identification ?? null,
+                    'organizationName' => $company->name ?? null,
+                    'npi' => $company->npi ?? null,
+                    'serviceProviderNumber' => $company?->sevices_number ?? null,
+                    'providerCode' => 'AD',
+                    'referenceIdentification' => $company?->reference_identification ?? null,
                 ],
                 'subscriber' => [
                     'memberId' => $insurancePolicy->subscriber->member_id ?? null,
-                    'firstName' => $insurancePolicy->subscriber->first_name ?? $patient->user->profile->first_name,
-                    'lastName' => $insurancePolicy->subscriber->last_name ?? $patient->user->profile->last_name,
-                    'gender' => $insurancePolicy->subscriber ? null : strtoupper($patient->user->profile->sex),
-                    'dateOfBirth' => $insurancePolicy->subscriber ? null : str_replace('-', '', $patient->user->profile->date_of_birth),
-                    'ssn' => $insurancePolicy->subscriber->ssn ?? $patient->user->profile->ssn,
+                    'firstName' => $insurancePolicy->subscriber->first_name ?? $patient->profile->first_name,
+                    'lastName' => $insurancePolicy->subscriber->last_name ?? $patient->profile->last_name,
+                    'gender' => $insurancePolicy->subscriber ? null : strtoupper($patient->profile->sex),
+                    'dateOfBirth' => $insurancePolicy->subscriber ? null : str_replace('-', '', $patient->profile->date_of_birth),
+                    'ssn' => $insurancePolicy->subscriber->ssn ?? $patient->profile->ssn,
                     'idCard' => $insurancePolicy->subscriber->id_card ?? null,
                 ],
                 'dependents' => [
                     [
-                        'firstName' => $patient->user->profile->first_name,
-                        'lastName' => $patient->user->profile->last_name,
-                        'gender' => strtoupper($patient->user->profile->sex),
-                        'dateOfBirth' => str_replace('-', '', $patient->user->profile->date_of_birth),
+                        'firstName' => $patient->profile->first_name,
+                        'lastName' => $patient->profile->last_name,
+                        'gender' => strtoupper($patient->profile->sex),
+                        'dateOfBirth' => str_replace('-', '', $patient->profile->date_of_birth),
                         'groupNumber' => $insurancePolicy->subscriber->group_number ?? null,
                     ],
                 ],
@@ -66,10 +72,10 @@ final class GetCheckStatusAction
             ];
 
             $response = Http::withToken($token)->acceptJson()->post(
-                $data[env('CHANGEHC_CONNECTION', 'sandbox')]['url'],
-                $data[env('CHANGEHC_CONNECTION', 'sandbox')]['body'] ?? $dataReal
+                config("claim.connections.url_status"),
+                $body
             );
-            $responseData = json_decode($response->body());
+            $responseData = json_decode($response->body(), true);
 
             return [
                 'claim_id' => $claim->id,
@@ -78,13 +84,3 @@ final class GetCheckStatusAction
         });
     }
 }
-$response = Http::acceptJson()
-    ->post(
-        config('claim.connections.url_token'),
-        [
-            'client_id' => config('claim.connections.client_id'),
-            'client_secret' => config('claim.connections.client_secret'),
-            'grant_type' => 'client_credentials',
-        ]
-    );
-$responseData = json_decode($response->body(), true);
