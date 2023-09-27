2bccc3c561d0215d3e7cd2323b0ac3b1e97f69c9
maikel jose <mb@ciph3r.co>
2023-09-26 22:50:51 -0400

    MCC-1678 change tags to classification enum

--- app/Models/Reports/Report.php
+++ app/Models/Reports/Report.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models\Reports;
 
+use App\Enums\Reports\ClassificationType;
 use App\Enums\Reports\ReportType;
 use App\Models\BillingCompany;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
@@ -19,19 +20,20 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property string $use
  * @property string $description
  * @property ReportType $type
- * @property array $tags
  * @property array $configuration
  * @property string $range
  * @property bool $favorite
  * @property int|null $billing_company_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property ClassificationType $clasification
  * @property BillingCompany|null $billingCompany
  *
  * @method static \Illuminate\Database\Eloquent\Builder|Report newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Report newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Report query()
  * @method static \Illuminate\Database\Eloquent\Builder|Report whereBillingCompanyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Report whereClasification($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Report whereConfiguration($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Report whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Report whereDescription($value)
@@ -39,7 +41,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @method static \Illuminate\Database\Eloquent\Builder|Report whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Report whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Report whereRange($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Report whereTags($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Report whereType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Report whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Report whereUse($value)
@@ -58,7 +59,7 @@ final class Report extends Model
         'description',
         'type',
         'range',
-        'tags',
+        'clasification',
         'configuration',
         'favorite',
         'billing_company_id',
@@ -67,7 +68,7 @@ final class Report extends Model
     /** @var array<key, string> */
     protected $casts = [
         'type' => ReportType::class,
-        'tags' => 'json',
+        'clasification' => ClassificationType::class,
         'configuration' => 'json',
         'favorite' => 'boolean',
     ];
