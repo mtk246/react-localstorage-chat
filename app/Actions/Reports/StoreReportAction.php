e1fb44683cd175301fc3b8d23aa058d13fbfca79
maikel jose <mb@ciph3r.co>
2023-09-26 22:51:56 -0400

    MCC-1678 update action tags to classification

--- app/Actions/Reports/StoreReportAction.php
+++ app/Actions/Reports/StoreReportAction.php
@@ -20,7 +20,7 @@ final class StoreReportAction
                     'description' => $report->getDescription(),
                     'type' => $report->getType(),
                     'range' => $report->getRange(),
-                    'tags' => $report->getTags(),
+                    'clasification' => $report->getClasification(),
                     'configuration' => $report->getConfiguration()->toArray(),
                     'favorite' => false,
                 ]), function (Report $reportModel) use ($report): void {
