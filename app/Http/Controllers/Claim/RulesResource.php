b8b0d3c9c7cdc722506d96914743fc5acfe5ab8e
maikel jose <mb@ciph3r.co>
2023-10-08 10:11:35 -0400

    add types controller update get all params

--- app/Http/Controllers/Claim/RulesResource.php
+++ app/Http/Controllers/Claim/RulesResource.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Claim\StoreRulesRequest;
 use App\Http\Requests\Claim\UpdateRulesRequest;
 use App\Http\Resources\Claim\RuleResource;
+use App\Http\Resources\Enums\TypeResource;
 use App\Models\Claims\Rules;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -25,9 +26,14 @@ final class RulesResource extends Controller
         }));
     }
 
+    public function getTypes(): JsonResponse
+    {
+        return response()->json(TypeResource::collection(ClaimType::cases()));
+    }
+
     public function index(Request $request, GetClaimRuleAction $getRule): JsonResponse
     {
-        return response()->json($getRule->getAll($request->user()->billing_company_id));
+        return response()->json($getRule->getAll($request));
     }
 
     public function store(StoreRulesRequest $request, StoreClaimRuleAction $storeRule): JsonResponse
