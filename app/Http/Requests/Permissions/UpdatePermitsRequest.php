7072a6ec5ccab3315c4360aa9917b39e732c97ca
maikel jose <mb@ciph3r.co>
2023-10-17 08:38:37 -0400

    MCC-1688 delete un nedded cast for validation MCC-1688 add validation for exit model

--- app/Http/Requests/Permissions/UpdatePermitsRequest.php
+++ app/Http/Requests/Permissions/UpdatePermitsRequest.php
@@ -4,21 +4,15 @@ declare(strict_types=1);
 
 namespace App\Http\Requests\Permissions;
 
-use App\Http\Casts\Permissions\PermissionWrapper;
-use App\Http\Requests\Traits\HasCastedClass;
 use Illuminate\Foundation\Http\FormRequest;
 
 final class UpdatePermitsRequest extends FormRequest
 {
-    use HasCastedClass;
-
-    protected string $castedClass = PermissionWrapper::class;
-
     /** @return array<string, mixed> */
     public function rules()
     {
         return [
-            'permits' => ['required', 'array'],
+            'permissions' => 'required|array|exists:permissions,id',
         ];
     }
 }
