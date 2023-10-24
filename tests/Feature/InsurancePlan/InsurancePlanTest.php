35b1e53505f650fc9804ea9008d0ca4a446eb053
minthukyaw <minthukyaw454@gmail.com>
2023-10-24 09:25:57 +0630

    [php] fix unwant imports

--- tests/Feature/InsurancePlan/InsurancePlanTest.php
+++ tests/Feature/InsurancePlan/InsurancePlanTest.php
@@ -4,23 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Feature\InsurancePlan;
 
-use App\Actions\InsurancePlan\AddCopays;
-use App\Actions\InsurancePlan\AddContractFees;
-use App\Actions\InsurancePlan\GetInsurancePlan;
-use App\Actions\InsurancePlan\GetInsurancePlanAction;
 use App\Http\Controllers\InsurancePlanController;
-use App\Http\Requests\ChangeStatusInsurancePlanRequest;
-use App\Http\Requests\InsurancePlan\AddContractFeesRequest;
-use App\Http\Requests\InsurancePlan\AddCopaysRequest;
-use App\Http\Requests\InsurancePlan\CreateRequest;
-use App\Http\Requests\InsurancePlan\UpdateRequest;
-use App\Http\Resources\InsurancePlan\InsurancePlanByPayerResource;
-use App\Models\InsurancePlan;
-use App\Models\ClearingHouse\AvailablePayer;
-use App\Services\ClearingHouseService;
 use App\Repositories\InsurancePlanRepository;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\JsonResponse;
 use Tests\TestCase;
