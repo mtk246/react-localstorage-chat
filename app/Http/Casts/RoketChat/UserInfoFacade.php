8405eaa10bdde0f85c29f7dd7b03e3a529d8c87e
maikel jose <mb@ciph3r.co>
2023-10-31 17:32:04 -0400

    MCC-675 fixt laravel pint

--- app/Http/Casts/RoketChat/UserInfoFacade.php
+++ app/Http/Casts/RoketChat/UserInfoFacade.php
@@ -6,7 +6,7 @@ namespace App\Http\Casts\RoketChat;
 
 use App\Http\Casts\CastsRequest;
 
+/** @todo: add facade getters, its whas not necesary for now but its required in future */
 final class UserInfoFacade extends CastsRequest
 {
-    //
 }
