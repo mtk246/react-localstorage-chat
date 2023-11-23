07c147cd717fd152cb5a241a682e15c7aed2f7b3
maikel jose <mb@ciph3r.co>
2023-11-23 05:57:33 -0400

    MCC-1789 add action for store batch data

---
+++ app/Actions/Payments/StoreBachAction.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Payments;
+
+use App\Enums\Payments\MethodType;
+use App\Http\Casts\Payments\PaymentWrapper;
+use App\Http\Casts\Payments\StoreBatchWrapper;
+use App\Http\Resources\Payments\BatchResource;
+use App\Models\Payments\Batch;
+use App\Models\Payments\Payment;
+use Illuminate\Support\Facades\DB;
+
+final class StoreBachAction
+{
+    public function invoke(StoreBatchWrapper $request): BatchResource
+    {
+        return DB::transaction(function () use ($request): BatchResource {
+            return new BatchResource(tap(Batch::query()->create($request->getBatchData()), function (Batch $batch) use ($request): void {
+                $request->getPaymentsData()->each(function (PaymentWrapper $paymentRequest) use ($batch): void {
+                    $paymentData = array_merge($paymentRequest->getPaymentdata(), [
+                        'payment_batch_id' => $batch->id,
+                    ]);
+                    $payment = Payment::query()->create($paymentData);
+
+                    if (MethodType::COMPLETED === $paymentRequest->getMethod()) {
+                        $payment->card()->create($paymentRequest->getCardData());
+                    }
+
+                    if ($paymentRequest->hasEob()) {
+                        $paymentRequest->getEobs()->each(function ($eobRequest) use ($payment) {
+                            $payment->eobs()->create($eobRequest->getEobData());
+                        });
+                    }
+                });
+            }));
+        });
+    }
+}
