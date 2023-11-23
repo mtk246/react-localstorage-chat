c81307ce14415068a4e91e8172a36a5fb17ebed2
maikel jose <mb@ciph3r.co>
2023-11-23 07:42:51 -0400

    MCC-1789 add basic resources for payments

---
+++ app/Http/Resources/Payments/BatchResource.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Payments;
+
+use App\Http\Resources\Company\CompanyResource;
+use App\Models\Payments\Batch;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**  @property Batch $resource */
+final class BatchResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->resource->name,
+            'posting_date' => $this->resource->posting_date,
+            'currency' => $this->resource->currency,
+            'amount' => $this->resource->amount->getAmount(),
+            'status' => $this->resource->status,
+            'payments' => PaymentResource::collection($this->resource->payments),
+            'created_at' => $this->resource->created_at,
+            'updated_at' => $this->resource->updated_at,
+            'company' => new CompanyResource($this->resource->company),
+            'billing_company' => $this->resource->billingCompany,
+        ];
+    }
+}
---
+++ app/Http/Resources/Payments/PaymentResource.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources\Payments;
+
+use App\Models\Payments\Payment;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**  @property Payment $resource */
+final class PaymentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'source' => $this->resource->source,
+            'payment_date' => $this->resource->payment_date,
+            'total_amount' => $this->resource->total_amount->getAmount(),
+            'payment_method' => $this->resource->payment_method,
+            'reference' => $this->resource->reference,
+            'card_number' => $this->resource->card?->card_number,
+            'expiration_date' => $this->resource->card?->expiration_date,
+            'statement' => $this->resource->statement,
+            'note' => $this->resource->note,
+            'eobs' => new EobResource($this->resource->eobs),
+            'insurance_company' => $this->resource->insuranceCompany,
+        ];
+    }
+}
