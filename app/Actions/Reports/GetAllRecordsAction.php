3d1f240fa708129a1c5d23838f823e8b0c43f966
Eduardo Saes <es@ciph3r.co>
2023-11-25 19:55:40 -0400

    feat: table layout setting

--- app/Actions/Reports/GetAllRecordsAction.php
+++ app/Actions/Reports/GetAllRecordsAction.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\Actions\Reports;
 
 use App\Enums\Reports\TypeReportAll;
+use App\Facades\Pagination;
 use App\Http\Resources\Reports\AllRecordsResource;
 use App\Models\User;
-use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
@@ -15,14 +16,14 @@ final class GetAllRecordsAction
 {
     public function getAllPatient(string $module, User $user)
     {
-        $data = DB::transaction(function () use ($module, $user): Collection {
+        $data = DB::transaction(function () use ($module, $user): LengthAwarePaginator {
             return DB::table(TypeReportAll::from($module)->getText())
                 ->when(
                     Gate::denies('is-admin'),
                     fn ($query) => $query->where('billing_id', $user->billing_company_id),
-                )->get();
+                )->orderBy(Pagination::sortBy(), Pagination::sortDesc())
+                ->paginate(Pagination::itemsPerPage());
         })->toArray();
-
         return new AllRecordsResource($data, TypeReportAll::from($module)->getName());
     }
 }
