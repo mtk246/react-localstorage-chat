a9af1754b4d0cfbe737f9b8bf3be2b71b601b088
Henry Paredes <henryp2804@gmail.com>
2023-11-28 14:46:50 -0400

    MCC-1932 Adjustments to the claim rules list

--- app/Actions/Claim/GetRulesListAction.php
+++ app/Actions/Claim/GetRulesListAction.php
@@ -12,29 +12,47 @@ final class GetRulesListAction
 {
     public function invoke(): Collection
     {
-        return collect(config('claim.formats'))->mapWithKeys(function ($item, $key) {
-            $name = ClaimType::tryFrom($key)->getName();
-            $formated = collect($item)->map(function ($item) use ($name) {
-                return collect($item)
-                    ->mapToGroups(function ($item, $key) {
-                        $group = Str::onlyNumbers($key);
-                        $item['code'] = $key;
+        $claimType = match (request()->input('type')) {
+            'professional' => ClaimType::PROFESSIONAL,
+            'institutional' => ClaimType::INSTITUTIONAL,
+            default => null,
+        };
 
-                        return [$group => $item];
-                    })
-                    ->map(function ($item, $key) use ($name) {
-                        return count($item) > 1
-                            ? [
-                                'code' => $key,
-                                'type' => 'group',
-                                'description' => __("claim.rules.{$name}.group.{$key}"),
-                                'values' => $item->values(),
-                            ]
-                            : $item->first();
-                    })->values();
+        if ($claimType) {
+            $items = config('claim.formats.'.$claimType->value);
+            $name = $claimType->getName();
+
+            return $this->getRuleListFormated($items, $name);
+        } else {
+            return collect(config('claim.formats'))->mapWithKeys(function ($item, $key) {
+                $name = ClaimType::tryFrom($key)->getName();
+                $formated = $this->getRuleListFormated($item, $name);
+
+                return [$name => $formated];
             });
+        }
+    }
 
-            return [$name => $formated];
+    private function getRuleListFormated(array $item, string $name): Collection
+    {
+        return collect($item)->map(function ($item) use ($name) {
+            return collect($item)
+                ->mapToGroups(function ($item, $key) {
+                    $group = Str::onlyNumbers($key);
+                    $item['code'] = $key;
+
+                    return [$group => $item];
+                })
+                ->map(function ($item, $key) use ($name) {
+                    return count($item) > 1
+                        ? [
+                            'code' => $key,
+                            'type' => 'group',
+                            'description' => __("claim.rules.{$name}.group.{$key}"),
+                            'values' => $item->values(),
+                        ]
+                        : $item->first();
+                })->values();
         });
     }
 }
