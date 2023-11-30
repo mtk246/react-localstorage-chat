bdf433a4ae1c9fb7c396ac54b9c636e3b08a94ba
Henry Paredes <henryp2804@gmail.com>
2023-11-30 08:40:05 -0400

    MCC-1932 Settings in json configuration file for professional claims submitters

--- config/Claims/Professionals/json.php
+++ config/Claims/Professionals/json.php
@@ -25,12 +25,81 @@ return [
         'description' => null,
         'value' => 'claim:usageIndicator',
     ],
+    'submitter.organizationName' => [
+        'type' => RuleType::SINGLE->value,
+        'description' => 'Organization name',
+        'value' => ['id' => 'claim:submitter.organizationName', 'name' => 'Submitter organization name'],
+        'values' => [
+            'id' => 'claim:submitter.organizationName',
+            'name' => 'Submitter organization name',
+        ],
+    ],
+    'submitter.lastName' => [
+        'type' => RuleType::NONE->value,
+        'description' => 'Submitter last name',
+        'value' => [],
+        'values' => [],
+    ],
+    'submitter.firstName' => [
+        'type' => RuleType::NONE->value,
+        'description' => 'Submitter first name',
+        'value' => [],
+        'values' => [],
+    ],
+    'submitter.middleName' => [
+        'type' => RuleType::NONE->value,
+        'description' => 'Submitter middle name',
+        'value' => [],
+        'values' => [],
+    ],
+    'submitter.contactInformation.name' => [
+        'type' => RuleType::SINGLE->value,
+        'description' => 'Submitter contact information name',
+        'value' => ['id' => 'claim:submitter.contactInformation.name', 'name' => 'Submitter contact information name'],
+        'values' => [
+            'id' => 'claim:submitter.contactInformation.name',
+            'name' => 'Submitter contact information name',
+        ],
+    ],
+    'submitter.contactInformation.phoneNumber' => [
+        'type' => RuleType::SINGLE->value,
+        'description' => 'Submitter contact information phone number',
+        'value' => ['id' => 'claim:submitter.contactInformation.phoneNumber', 'name' => 'Submitter contact information phone number'],
+        'values' => [
+            'id' => 'claim:submitter.contactInformation.phoneNumber',
+            'name' => 'Submitter contact information phone number',
+        ],
+    ],
+    'submitter.contactInformation.faxNumber' => [
+        'type' => RuleType::SINGLE->value,
+        'description' => 'Submitter contact information fax number',
+        'value' => ['id' => 'claim:submitter.contactInformation.faxNumber', 'name' => 'Submitter contact information fax number'],
+        'values' => [
+            'id' => 'claim:submitter.contactInformation.faxNumber',
+            'name' => 'Submitter contact information fax number',
+        ],
+    ],
+    'submitter.contactInformation.email' => [
+        'type' => RuleType::SINGLE->value,
+        'description' => 'Submitter contact information email',
+        'value' => ['id' => 'claim:submitter.contactInformation.email', 'name' => 'Submitter contact information email'],
+        'values' => [
+            'id' => 'claim:submitter.contactInformation.email',
+            'name' => 'Submitter contact information email',
+        ],
+    ],
+    'submitter.contactInformation.phoneExtension' => [
+        'type' => RuleType::NONE->value,
+        'description' => 'Submitter contact information phone extension',
+        'value' => [],
+        'values' => [],
+    ],
     'submitter' => [
         'type' => RuleType::SINGLE_ARRAY->value,
         'description' => null,
         'value' => 'claim:submitter',
     ],
-    'receiver' => [
+    /**'receiver' => [
         'type' => RuleType::SINGLE_ARRAY->value,
         'description' => null,
         'value' => 'claim:receiver',
@@ -54,7 +123,7 @@ return [
         'type' => RuleType::SINGLE_ARRAY->value,
         'description' => null,
         'value' => 'claim:payToAddress',
-    ],
+    ],**/
     /*'payToPlan' => [
         'type' => RuleType::SINGLE_ARRAY->value,
         'value' => 'claim:payToPlan',
@@ -63,7 +132,7 @@ return [
         'type' => RuleType::SINGLE_ARRAY->value,
         'value' => 'claim:payerAddress',
     ],*/
-    'billing' => [
+    /*'billing' => [
         'type' => RuleType::SINGLE_ARRAY->value,
         'description' => null,
         'value' => 'claim:billing',
@@ -72,7 +141,7 @@ return [
         'type' => RuleType::SINGLE_ARRAY->value,
         'description' => null,
         'value' => 'claim:referring',
-    ],
+    ],**/
     /*'rendering' => [
         'type' => RuleType::SINGLE_ARRAY->value,
         'value' => 'claim:rendering',
