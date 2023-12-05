a1ccc1d16c16cf13652221297061204f71f8924c
maikel jose <mb@ciph3r.co>
2023-12-05 11:48:58 -0400

    update insurance plans to groups

--- app/Http/Resources/Claim/RuleResource.php
+++ app/Http/Resources/Claim/RuleResource.php
@@ -6,7 +6,9 @@ namespace App\Http\Resources\Claim;
 
 use App\Enums\Claim\RuleFormatType;
 use App\Models\Claims\Rules;
+use App\Models\InsurancePlan;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Collection;
 
 /** @property Rules $resource */
 final class RuleResource extends JsonResource
@@ -22,7 +24,27 @@ final class RuleResource extends JsonResource
             'id' => $this->resource->id,
             'name' => $this->resource->name,
             'description' => $this->resource->description,
-            'insurance_plans' => $this->resource->insurancePlans,
+            'insurance_plans' => $this->resource->insurancePlans->reduce(function (Collection $carry, InsurancePlan $plan) {
+                if (!$carry->contains(fn ($item) => $item['id'] === $plan->insurance_company_id)) {
+                    $carry->push([
+                        'id' => $plan->insurance_company_id,
+                        'name' => $plan->insuranceCompany->name,
+                        'plans' => collect(),
+                    ]);
+                }
+
+                $carry->first(fn ($item) => $item['id'] === $plan->insurance_company_id)['plans']->push([
+                    'id' => $plan->id,
+                    'name' => $plan->name,
+                    'abbreviation' => $plan
+                        ->abbreviations
+                        ->where('billing_company_id', $this->resource->billing_company_id)
+                        ->first()
+                        ?->abbreviation ?? '',
+                ]);
+
+                return $carry;
+            }, collect()),
             'format' => $this->resource->format,
             'responsibilities' => $this->resource->typesOfResponsibilities?->map(fn ($type) => [
                 'id' => $type->id,
