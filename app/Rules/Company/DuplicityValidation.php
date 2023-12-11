36039d81e1f4a8b24efae30c52429dbfb02e77d3
Ivan Castillo <ic@ciph3r.co>
2023-12-11 07:56:28 -0400

    MCC-1909 Applied laravel pint

--- app/Rules/Company/DuplicityValidation.php
+++ app/Rules/Company/DuplicityValidation.php
@@ -8,13 +8,11 @@ use Illuminate\Contracts\Validation\Rule;
 
 final class DuplicityValidation implements Rule
 {
-
-    //procedure_id,revenue_code_id, modifier_ids
+    // procedure_id,revenue_code_id, modifier_ids
     public function passes($attribute, $value): bool
     {
-
         $collection = collect($value)->map(
-            fn ($service) => ($service['procedure_id'] ?? null). '-' . ($service['revenue_code_id'] ?? null). '-' . ($service['modifier_id'] ?? null)
+            fn ($service) => ($service['procedure_id'] ?? null).'-'.($service['revenue_code_id'] ?? null).'-'.($service['modifier_id'] ?? null)
         );
 
         return $collection->duplicates()->count() <= 0 ?? false;
