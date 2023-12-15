2149bd2dfe92579fc1bf317f8e1bb52d1f888159
Min Thu  Kyaw <minthukyaw@Mins-MacBook-Air.local>
2023-12-15 18:43:51 +0630

    [php] refactor claim search and modify status filter after miliesearch is replaced

--- app/Actions/Claim/GetDenialAction.php
+++ app/Actions/Claim/GetDenialAction.php
@@ -9,11 +9,12 @@ use App\Http\Casts\Claims\DenialTrackingWrapper;
 use App\Http\Resources\Claim\DenialBodyResource;
 use App\Models\Claims\Claim;
 use App\Models\Claims\ClaimStatus;
-use App\Models\Claims\ClaimSubStatus;
 use App\Models\Claims\DenialTracking;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Validator;
+use Meilisearch\Endpoints\Indexes;
 
 final class GetDenialAction
 {
@@ -26,69 +27,49 @@ final class GetDenialAction
 
     public function all(Claim $claim, Request $request, $status, $subStatus)
     {
-        $query = $claim->query();
-
-        if (count($status) > 0) {
+        $claimsQuery = Claim::when(count($status) > 0, function ($query) use ($status) {
             $query->whereHas('claimStatusClaims', function ($query) use ($status) {
                 $query
                     ->where('claim_status_claim.claim_status_type', ClaimStatus::class)
                     ->whereIn('claim_status_claim.claim_status_id', $status)
                     ->whereRaw('claim_status_claim.created_at = (SELECT MAX(created_at) FROM claim_status_claim WHERE claim_status_claim.claim_id = claims.id)');
             });
-        }
+        })
+            ->search(
+                $request->query('query', ''),
+                function (Indexes $searchEngine, string $query, array $options) use ($request, $status) {
+                    $config = config('scout.meilisearch.index-settings.'.Claim::class);
 
-        if (count($subStatus) > 0) {
-            $query->orWhereHas('claimStatusClaims', function ($query) use ($subStatus) {
-                $query
-                    ->where('claim_status_claim.claim_status_type', ClaimSubStatus::class)
-                    ->whereIn('claim_status_claim.claim_status_id', $subStatus)
-                    ->whereRaw('claim_status_claim.created_at = (SELECT MAX(created_at) FROM claim_status_claim WHERE claim_status_claim.claim_id = claims.id)');
-            });
-        }
+                    if (isset($request->sortBy) && in_array($request->sortBy, $config['sortableAttributes'])) {
+                        $options['sort'] = [$request->sortBy.':'.Pagination::sortDesc()];
+                    }
 
-        if ($request->user()->isAdmin()) {
-            $query->where('billing_company_id', $request->user()->billing_company_id);
-        }
+                    if (isset($request->filter)) {
+                        $options['filter'] = $request->filter;
+                    }
 
-        $query->when(
-            !empty($request->query('query')) && '{}' !== $request->query('query'),
-            fn ($query) => $query->search($request->query('query'))
-        )->when(
-            !empty($request->patient_id),
-            fn ($query) => $query->whereHas('demographicInformation', function ($query) use ($request) {
-                $query->where('patient_id', $request->patient_id);
-            })
-        )->with([
-            'demographicInformation',
-            'service',
-            'insurancePolicies',
-            'denialTrackings',
-            'claimTransmissionResponses',
-            'claimStatusClaims' => function ($query) {
-                $query->whereNotIn('claim_status_id', [1, 2, 7]);
-            },
-        ]);
+                    if (isset($request->status)) {
+                        $statusFilter = is_array($request->status) ? $request->status : [$request->status];
+                        $options['filter'] = [];
 
-        if ('insurance_plan' === $request->sortBy) {
-            $query->join('claim_insurance_policy', 'claim_insurance_policy.claim_id', '=', 'claims.id')
-                ->join('insurance_policies', 'insurance_policies.id', '=', 'claim_insurance_policy.insurance_policy_id')
-                ->orderBy('insurance_policies.id', $request->sortDesc ? 'desc' : 'asc')
-                ->select('claims.*');
-        }
+                        foreach ($statusFilter as $status) {
+                            $options['filter'][] = 'status:'.$status;
+                        }
+                    }
 
-        $claimsQuery = $query->paginate(Pagination::itemsPerPage());
+                    return $searchEngine->search($query, $options);
+                }
+            )
+            ->when(
+                Gate::denies('is-admin'),
+                fn ($query) => $query->where('billing_company_id', $request->user()->billing_company_id),
+            )
+            ->paginate(Pagination::itemsPerPage());
 
         $data = [
             'data' => DenialBodyResource::collection(
-                collect($claimsQuery->items())->reject(function ($item) use ($request) {
+                collect($claimsQuery->items())->reject(function ($item) {
                     $status = (new DenialBodyResource($item))->getStatus();
-
-                    $query = $request->query('query');
-                    if (!empty($query)) {
-                        $patientFirstName = $item->demographicInformation->patient->profile->first_name ?? '';
-
-                        return 1 === $status->id || 2 === $status->id || 7 === $status->id || !str_contains(strtolower($patientFirstName), strtolower($query));
-                    }
 
                     return 1 === $status->id || 2 === $status->id || 7 === $status->id;
                 })
