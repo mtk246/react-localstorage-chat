f115288088b301ef692810a042ac126b546587f4
Henry Paredes <henryp2804@gmail.com>
2023-12-20 18:04:52 -0400

    MCC-1932 Fix list config claim file

--- config/Claims/Professionals/file.php
+++ config/Claims/Professionals/file.php
@@ -67,7 +67,7 @@ return [
             ],
         ],
     ],
-    '1' => [
+    '1.0' => [
         'type' => RuleType::SINGLE->value,
         'description' => 'Type of insurance',
         'value' => ['name' => 'Type code of insurance', 'id' => 'insType:code'],
@@ -432,7 +432,7 @@ return [
             ],
         ],
     ],
-    '9' => [
+    '9.0' => [
         'type' => RuleType::MULTIPLE->value,
         'length' => 30,
         'description' => 'Secondary policy subscriber',
@@ -1128,7 +1128,7 @@ return [
             ],
         ],
     ],
-    '21' => [
+    '21.0' => [
         'type' => RuleType::SINGLE->value,
         'description' => 'Claim diagnosis ICD indicator',
         'value' => ['name' => 'ICD indicator', 'id' => 'demograficInformation:|0'],
