ed9c8f98a4f5488228e1790415e0112cc6dbbbe9
Min Thu  Kyaw <minthukyaw@Mins-MacBook-Air.local>
2023-12-20 22:38:37 +0630

    [php] fix pint

--- app/Models/Payments/Eob.php
+++ app/Models/Payments/Eob.php
@@ -7,8 +7,8 @@ namespace App\Models\Payments;
 use App\Models\Claims\Claim;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Support\Collection;
 
