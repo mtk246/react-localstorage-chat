69b426ef9558cc8a44d2e25e08d14305036fcd99
maikel jose <mb@ciph3r.co>
2023-12-26 17:29:11 -0400

    fixt wrong default value

--- app/Http/Controllers/ClaimPreviewController.php
+++ app/Http/Controllers/ClaimPreviewController.php
@@ -20,7 +20,7 @@ final class ClaimPreviewController extends Controller
     public function show(Request $request, ClaimPreviewService $preview, GetClaimPreviewAction $claimPreview)
     {
         $id = $request->id ?? null;
-        $claim = $request->get('default', true)
+        $claim = $request->get('default', false)
             ? Claim::query()->with(['insurancePolicies'])->where('type', $request->get('type', ClaimType::INSTITUTIONAL->value))->first()
             : Claim::query()->with(['insurancePolicies'])->find($id);
     
