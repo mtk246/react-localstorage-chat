3cfe562428054dd23e09632ef1b3b497c5a09914
maikel jose <mb@ciph3r.co>
2024-01-14 18:15:31 -0400

    MCC-2074 update vlidator MCC-2074 add new key array

--- app/Http/Requests/Claim/StoreRulesRequest.php
+++ app/Http/Requests/Claim/StoreRulesRequest.php
@@ -21,7 +21,9 @@ final class StoreRulesRequest extends FormRequest
     public function rules()
     {
         return [
-            'insurance_plan_ids' => 'required|array',
+            'insurance_company_ids' => 'nullable|array',
+            'insurance_company_ids.*' => 'required|integer|exists:insurance_companies,id',
+            'insurance_plan_ids' => 'nullable|array',
             'insurance_plan_ids.*' => 'required|integer|exists:insurance_plans,id',
             'name' => 'required|string',
             'format' => [
