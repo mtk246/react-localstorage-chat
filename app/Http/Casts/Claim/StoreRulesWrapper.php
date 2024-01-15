ad3a238af4b64656a42a04bb9ec3965660613301
maikel jose <mb@ciph3r.co>
2024-01-15 15:54:56 -0400

    add company to wrapper

--- app/Http/Casts/Claim/StoreRulesWrapper.php
+++ app/Http/Casts/Claim/StoreRulesWrapper.php
@@ -32,6 +32,11 @@ final class StoreRulesWrapper extends CastsRequest
             : $this->user->billingCompanies->first()?->id;
     }
 
+    public function getCompanies(): array
+    {
+        return $this->getArray('company_ids');
+    }
+
     public function getInsuranceCompanies(): array
     {
         return $this->getArray('insurance_company_ids');
