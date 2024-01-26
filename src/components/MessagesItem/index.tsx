5a0392f703cb1487d2aaf2e4c8589172790e061e
Min Thu  Kyaw <minthukyaw@Mins-MacBook-Air.local>
2024-01-26 09:22:29 -0500

    [tsx] exchange sender and receipient places

--- src/components/MessagesItem/index.tsx
+++ src/components/MessagesItem/index.tsx
@@ -12,17 +12,17 @@ const Message: React.FC<Props> = (props) => {
             {
                 owner ? (
                     <li className="clearfix pb-3">
-                        <div className="message-data">
-                            <span className="font-weight-bold">{props.sender}</span>
-                        </div>
-                        <div className="badge badge-primary text-wrap text-break" style={{padding: '15px', fontSize: 16}}>{props.children}</div>                                    
-                    </li> 
-                ) : (
-                    <li className="clearfix pb-3">
                         <div className="message-data text-right">
                             <span className="font-weight-bold">{props.sender}</span>
                         </div>
-                        <div className="badge badge-secondary text-wrap text-break float-right"  style={{padding: '15px', fontSize: 16, background: '#ebebeb', color: '#6c757d'}}>{props.children}</div>
+                        <div className="badge badge-primary text-wrap text-break float-right"  style={{padding: '15px', fontSize: 16}}>{props.children}</div>
+                    </li>
+                ) : (
+                    <li className="clearfix pb-3">
+                        <div className="message-data">
+                            <span className="font-weight-bold">{props.sender}</span>
+                        </div>
+                        <div className="badge badge-secondary text-wrap text-break" style={{padding: '15px', fontSize: 16, background: '#ebebeb', color: '#6c757d'}}>{props.children}</div>
                     </li>
                 )
             }
